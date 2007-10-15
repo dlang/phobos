b3e542b19b56bb1bd6d40f4b6187fb72517d5681
Walter Bright <walter@digitalmars.com>
2007-10-15 06:13:02 +0000

    stdio should be stdout

--- std/format.d
+++ std/format.d
@@ -38,6 +38,7 @@ module std.format;
 import std.stdarg;	// caller will need va_list
 
 private import std.utf;
+private import std.c.stdio;
 private import std.c.stdlib;
 private import std.c.string;
 private import std.string;
@@ -62,11 +63,7 @@ version (DigitalMarsC)
 	extern char* function(int c, int flags, int precision, real* pdval,
 	    char* buf, int* psl, int width) __pfloatfmt;
     }
-}
-else
-{
-    // Use C99 snprintf
-    extern (C) int snprintf(char* s, size_t n, const char* format, ...);
+    alias std.c.stdio._snprintf snprintf;
 }
 
 /**********************************************************************
