0d4be13d4d2e813b66c7c9e9989d2b9f92ccb05f
Andrei Alexandrescu <andrei@erdani.com>
2008-02-20 08:14:39 +0000

    Don Clugston's changes

--- std/math.d
+++ std/math.d
@@ -2116,7 +2116,7 @@ unittest
 bool approxEqual(T, U, V)(T lhs, U rhs, V maxRelDiff, V maxAbsDiff = 0)
 {
     static if (isArray!(T)) {
-        final n = lhs.length;
+        invariant n = lhs.length;
         static if (isArray!(U)) {
             // Two arrays
             assert(n == rhs.length);
