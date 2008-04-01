58749fe3fd0bcde601209fd24500dd9c40904069
Don Clugston <dclugston@googlemail.com>
2008-04-01 19:10:40 +0000

    * added numeric.findRoot. AFAIK this is faster than any previously published 1D general-purpose root finder, * added math.ieeeMean. Not yet public (may need a different name). Used  by findRoot. * reformatting for std.math -- 80 char width, etc.

--- std/numeric.d
+++ std/numeric.d
@@ -37,6 +37,7 @@ $(WEB erdani.org, Andrei Alexandrescu)
  */
 
 module std.numeric;
+import std.typecons;
 import std.math;
 version(unittest)
 {
@@ -84,5 +85,456 @@ unittest
     }
     invariant x = secantMethod!(f)(0f, 1f);
     assert(approxEqual(x, 0.865474));
+}
+
+
+private:
+// Return true if a and b have opposite sign.
+bool oppositeSigns(T)(T a, T b)
+{
+    // Use signbit() if available, otherwise check the signs.
+    static if (is(typeof(signbit(a)))) {    
+        return (signbit(a) ^ signbit(b))!=0;
+    } else return (a>0 && b<0) || (a>0 && b<0);
+}
+
+
+public:
+
+/**  Find a real root of the real function f(x) via bracketing.
+ *
+ * Given a range [a..b] such that f(a) and f(b) have opposite sign,
+ * returns the value of x in the range which is closest to a root of f(x).
+ * If f(x) has more than one root in the range, one will be chosen arbitrarily.
+ * If f(x) returns $(NAN), $(NAN) will be returned; otherwise, this algorithm
+ * is guaranteed to succeed. 
+ *  
+ * Uses an algorithm based on TOMS748, which uses inverse cubic interpolation 
+ * whenever possible, otherwise reverting to parabolic or secant
+ * interpolation. Compared to TOMS748, this implementation improves worst-case
+ * performance by a factor of more than 100, and typical performance by a factor
+ * of 2. For 80-bit reals, most problems require 8 - 15 calls to f(x) to achieve
+ * full machine precision. The worst-case performance (pathological cases) is 
+ * approximately twice the number of bits. 
+ *
+ * References: 
+ * "On Enclosing Simple Roots of Nonlinear Equations", G. Alefeld, F.A. Potra, 
+ *   Yixun Shi, Mathematics of Computation 61, pp733-744 (1993).
+ *   Fortran code available from www.netlib.org as algorithm TOMS478.
+ *
+ */
+T findRoot(T, R)(R delegate(T) f, T ax, T bx)
+{
+    auto r = findRoot(f, ax, bx, f(ax), f(bx), (Tuple!(T, T, R, R) r){ 
+         return r._1==nextUp(r._0); });
+    return fabs(r._2)<=fabs(r._3) ? r._0 : r._1;
+}
+
+/** Find root by bracketing, allowing termination condition to be specified
+ *
+ * Params:
+ * tolerance   Defines the termination condition. Return true when acceptable
+ *             bounds have been obtained.
+ */
+Tuple!(T, T, R, R) findRoot(T,R)(R delegate(T) f, T ax, T bx, R fax, R fbx,
+    bool delegate(Tuple!(T, T, R, R)  r) tolerance)
+in {
+    assert(ax<=bx, "Parameters ax and bx out of order.");
+    assert(ax<>=0 && bx<>=0, "Limits must not be NaN");
+    assert(oppositeSigns(fax,fbx), "Parameters must bracket the root.");
+}
+body {   
+// This code is (heavily) modified from TOMS748 (www.netlib.org). Some ideas
+// were borrowed from the Boost Mathematics Library.
+
+    T a = ax, b = bx, d;  // [a..b] is our current bracket.
+    R fa = fax, fb = fbx, fd; // d is the third best guess.       
+
+    // Test the function at point c; update brackets accordingly
+    void bracket(T c)
+    {
+        T fc = f(c);        
+        if (fc == 0) { // Exact solution
+            a = c;
+            fa = fc;
+            d = c;
+            fd = fc;
+            return;
+        }
+        // Determine new enclosing interval
+        if (oppositeSigns(fa, fc)) {
+            d = b;
+            fd = fb;
+            b = c;
+            fb = fc;
+        } else {
+            d = a;
+            fd = fa;
+            a = c;
+            fa = fc;
+        }
+    }
+
+   /* Perform a secant interpolation. If the result would lie on a or b, or if
+     a and b differ so wildly in magnitude that the result would be meaningless,
+     perform a bisection instead.
+    */
+    T secant_interpolate(T a, T b, T fa, T fb)
+    {
+        if (( ((a - b) == a) && b!=0) || (a!=0 && ((b - a) == b))) {
+            // Catastrophic cancellation
+            if (a == 0) a = copysign(0.0L, b);
+            else if (b == 0) b = copysign(0.0L, a);
+            else if (oppositeSigns(a, b)) return 0;
+            T c = ieeeMean(a, b); 
+            return c;
+        }
+       // avoid overflow
+       if (b - a > T.max)    return b / 2.0 + a / 2.0;
+       if (fb - fa > T.max)  return a - (b - a) / 2;
+       T c = a - (fa / (fb - fa)) * (b - a);
+       if (c == a || c == b) return (a + b) / 2;
+       return c;
+    }
+    
+    /* Uses 'numsteps' newton steps to approximate the zero in [a..b] of the
+       quadratic polynomial interpolating f(x) at a, b, and d.
+       Returns:         
+         The approximate zero in [a..b] of the quadratic polynomial.
+    */
+    T newtonQuadratic(int numsteps)
+    {
+        // Find the coefficients of the quadratic polynomial.
+        T a0 = fa;
+        T a1 = (fb - fa)/(b - a);
+        T a2 = ((fd - fb)/(d - b) - a1)/(d - a);
+    
+        // Determine the starting point of newton steps.
+        T c = oppositeSigns(a2, fa) ? a  : b;
+     
+        // start the safeguarded newton steps.
+        for (int i = 0; i<numsteps; ++i) {        
+            T pc = a0 + (a1 + a2 * (c - b))*(c - a);
+            T pdc = a1 + a2*((2.0 * c) - (a + b));
+            if (pdc == 0) return a - a0 / a1;
+            else c = c - pc / pdc;        
+        }
+        return c;    
+    }
+    
+    // On the first iteration we take a secant step:
+    if(fa != 0) {
+        bracket(secant_interpolate(a, b, fa, fb));
+    }
+    // Starting with the second iteration, higher-order interpolation can
+    // be used.
+    int itnum = 1;   // Iteration number    
+    int baditer = 1; // Num bisections to take if an iteration is bad.
+    T c, e;  // e is our fourth best guess
+    R fe;   
+whileloop:
+    while((fa != 0) && !tolerance(Tuple!(T, T, R, R)(a, b, fa, fb))) {        
+        T a0 = a, b0 = b; // record the brackets
+      
+        // Do two higher-order (cubic or parabolic) interpolation steps.
+        for (int QQ = 0; QQ < 2; ++QQ) {      
+            // Cubic inverse interpolation requires that 
+            // all four function values fa, fb, fd, and fe are distinct; 
+            // otherwise use quadratic interpolation.
+            bool distinct = (fa != fb) && (fa != fd) && (fa != fe) 
+                         && (fb != fd) && (fb != fe) && (fd != fe);
+            // The first time, cubic interpolation is impossible.
+            if (itnum<2) distinct = false;
+            bool ok = distinct;
+            if (distinct) {                
+                // Cubic inverse interpolation of f(x) at a, b, d, and e
+                real q11 = (d - e) * fd / (fe - fd);
+                real q21 = (b - d) * fb / (fd - fb);
+                real q31 = (a - b) * fa / (fb - fa);
+                real d21 = (b - d) * fd / (fd - fb);
+                real d31 = (a - b) * fb / (fb - fa);
+                      
+                real q22 = (d21 - q11) * fb / (fe - fb);
+                real q32 = (d31 - q21) * fa / (fd - fa);
+                real d32 = (d31 - q21) * fd / (fd - fa);
+                real q33 = (d32 - q22) * fa / (fe - fa);
+                c = a + (q31 + q32 + q33);
+                if (c!<>=0 || (c <= a) || (c >= b)) {
+                    // DAC: If the interpolation predicts a or b, it's 
+                    // probable that it's the actual root. Only allow this if
+                    // we're already close to the root.                
+                    if (c == a && a - b != a) {
+                        c = nextUp(a);
+                    }
+                    else if (c == b && a - b != -b) {
+                        c = nextDown(b);
+                    } else {
+                        ok = false;
+                    }
+                }
+            }
+            if (!ok) {
+               c = newtonQuadratic(distinct ? 3 : 2);
+               if(c!<>=0 || (c <= a) || (c >= b)) {
+                  // Failure, try a secant step:
+                  c = secant_interpolate(a, b, fa, fb);
+               }
+            }
+            ++itnum;                
+            e = d;
+            fe = fd;
+            bracket(c);
+            if((fa == 0) || tolerance(Tuple!(T, T, R, R)(a, b, fa, fb)))
+                break whileloop;
+            if (itnum == 2)
+                continue whileloop;
+        }
+        // Now we take a double-length secant step:
+        T u;
+        R fu;
+        if(fabs(fa) < fabs(fb)) {
+             u = a;
+             fu = fa;
+        } else {
+             u = b;
+             fu = fb;
+        }
+        c = u - 2 * (fu / (fb - fa)) * (b - a);
+        // DAC: If the secant predicts a value equal to an endpoint, it's
+        // probably false.      
+        if(c==a || c==b || c!<>=0 || fabs(c - u) > (b - a) / 2) {
+            if ((a-b) == a || (b-a) == b) {
+                if ( (a>0 && b<0) || (a<0 && b>0) ) c = 0;
+                else {
+                   if (a==0) c = ieeeMean(copysign(0.0L, b), b);
+                   else if (b==0) c = ieeeMean(copysign(0.0L, a), a);
+                   else c = ieeeMean(a, b);
+                }
+            } else {
+                c = a + (b - a) / 2;
+            }       
+        }
+        e = d;
+        fe = fd;
+        bracket(c);
+        if((fa == 0) || tolerance(Tuple!(T, T, R, R)(a, b, fa, fb)))
+            break;
+            
+        // We must ensure that the bounds reduce by a factor of 2 
+        // (DAC: in binary space!) every iteration. If we haven't achieved this
+        // yet (DAC: or if we don't yet know what the exponent is),
+        // perform a binary chop.
+
+        if( (a==0 || b==0 || 
+            (fabs(a) >= 0.5 * fabs(b) && fabs(b) >= 0.5 * fabs(a))) 
+            &&  (b - a) < 0.25 * (b0 - a0))  {
+                baditer = 1;        
+                continue;
+            }
+        // DAC: If this happens on consecutive iterations, we probably have a
+        // pathological function. Perform a number of bisections equal to the
+        // total number of consecutive bad iterations.
+        
+        if ((b - a) < 0.25 * (b0 - a0)) baditer=1;
+        for (int QQ = 0; QQ < baditer ;++QQ) {
+            e = d;
+            fe = fd;
+    
+            T w;
+            if ((a>0 && b<0) ||(a<0 && b>0)) w = 0;
+            else {
+                T usea = a;
+                T useb = b;
+                if (a == 0) usea = copysign(0.0L, b);
+                else if (b == 0) useb = copysign(0.0L, a);
+                w = ieeeMean(usea, useb);
+            }
+            bracket(w);
+        }
+        ++baditer;
+    }
+
+    if (fa == 0) { b = a; fb = fa; }
+    else if (fb == 0) { a = b; fa = fb; }
+    return Tuple!(T, T, R, R)(a, b, fa, fb);
+}
+
+unittest{
+    
+    int numProblems = 0;
+    int numCalls;
+    
+    void testFindRoot(real delegate(real) f, real x1, real x2) {
+        numCalls=0;
+        ++numProblems;
+        assert(x1<>=0 && x2<>=0);
+        auto result = findRoot(f, x1, x2, f(x1), f(x2),
+            (Tuple!(real, real, real, real) r){ return r._1==nextUp(r._0); });
+        
+        auto flo = f(result._0);
+        auto fhi = f(result._1);
+        if (flo!=0) {
+            assert(oppositeSigns(flo, fhi));
+        }
+    }
+    
+    // Test functions
+    real cubicfn (real x) {
+       ++numCalls;
+       if (x>float.max) x = float.max;
+       if (x<-double.max) x = -double.max;
+       // This has a single real root at -59.286543284815
+       return 0.386*x*x*x + 23*x*x + 15.7*x + 525.2;
+    }
+    // Test a function with more than one root.
+    real multisine(real x) { ++numCalls; return sin(x); }
+    testFindRoot( &multisine, 6, 90);
+    testFindRoot(&cubicfn, -100, 100);    
+    testFindRoot( &cubicfn, -double.max, real.max);
+    
+    
+/* Tests from the paper:
+ * "On Enclosing Simple Roots of Nonlinear Equations", G. Alefeld, F.A. Potra, 
+ *   Yixun Shi, Mathematics of Computation 61, pp733-744 (1993).
+ */
+    // Parameters common to many alefeld tests.
+    int n;
+    real ale_a, ale_b;
+
+    int powercalls = 0;
+    
+    real power(real x) {
+        ++powercalls;
+        ++numCalls;
+        return pow(x, n) + double.min;
+    }
+    int [] power_nvals = [3, 5, 7, 9, 19, 25];
+    // Alefeld paper states that pow(x,n) is a very poor case, where bisection
+    // outperforms his method, and gives total numcalls = 
+    // 921 for bisection (2.4 calls per bit), 1830 for Alefeld (4.76/bit), 
+    // 2624 for brent (6.8/bit)
+    // ... but that is for double, not real80.
+    // This poor performance seems mainly due to catastrophic cancellation, 
+    // which is avoided here by the use of ieeeMean().
+    // I get: 231 (0.48/bit).
+    // IE this is 10X faster in Alefeld's worst case
+    numProblems=0;
+    foreach(k; power_nvals) {
+        n = k;
+        testFindRoot(&power, -1, 10);
+    }
+    
+    int powerProblems = numProblems;
+
+    // Tests from Alefeld paper
+        
+    int [9] alefeldSums;
+    real alefeld0(real x){
+        ++alefeldSums[0];
+        ++numCalls;
+        real q =  sin(x) - x/2;
+        for (int i=1; i<20; ++i)
+            q+=(2*i-5.0)*(2*i-5.0)/((x-i*i)*(x-i*i)*(x-i*i));
+        return q;
+    }
+   real alefeld1(real x) {
+        ++numCalls;
+       ++alefeldSums[1];
+       return ale_a*x + exp(ale_b * x);
+   }
+   real alefeld2(real x) {
+        ++numCalls;
+       ++alefeldSums[2];
+       return pow(x, n) - ale_a;
+   }
+   real alefeld3(real x) {
+        ++numCalls;
+       ++alefeldSums[3];
+       return (1.0 +pow(1.0L-n, 2))*x - pow(1.0L-n*x, 2);
+   }
+   real alefeld4(real x) {
+        ++numCalls;
+       ++alefeldSums[4];
+       return x*x - pow(1-x, n);
+   }
+   
+   real alefeld5(real x) {
+        ++numCalls;
+       ++alefeldSums[5];
+       return (1+pow(1.0L-n, 4))*x - pow(1.0L-n*x, 4);
+   }
+   
+   real alefeld6(real x) {
+        ++numCalls;
+       ++alefeldSums[6];
+       return exp(-n*x)*(x-1.01L) + pow(x, n);
+   }
+   
+   real alefeld7(real x) {
+        ++numCalls;
+       ++alefeldSums[7];
+       return (n*x-1)/((n-1)*x);
+   }
+   numProblems=0;
+   testFindRoot(&alefeld0, PI_2, PI);
+   for (n=1; n<=10; ++n) {
+    testFindRoot(&alefeld0, n*n+1e-9L, (n+1)*(n+1)-1e-9L);
+   }
+   ale_a = -40; ale_b = -1;
+   testFindRoot(&alefeld1, -9, 31);
+   ale_a = -100; ale_b = -2;
+   testFindRoot(&alefeld1, -9, 31);
+   ale_a = -200; ale_b = -3;
+   testFindRoot(&alefeld1, -9, 31);
+   int [] nvals_3 = [1, 2, 5, 10, 15, 20];
+   int [] nvals_5 = [1, 2, 4, 5, 8, 15, 20];
+   int [] nvals_6 = [1, 5, 10, 15, 20];
+   int [] nvals_7 = [2, 5, 15, 20];
+  
+    for(int i=4; i<12; i+=2) {
+       n = i;
+       ale_a = 0.2;
+       testFindRoot(&alefeld2, 0, 5);
+       ale_a=1;
+       testFindRoot(&alefeld2, 0.95, 4.05);
+       testFindRoot(&alefeld2, 0, 1.5);       
+    }
+    foreach(i; nvals_3) {
+        n=i;
+        testFindRoot(&alefeld3, 0, 1);
+    }
+    foreach(i; nvals_3) {
+        n=i;
+        testFindRoot(&alefeld4, 0, 1);
+    }
+    foreach(i; nvals_5) {
+        n=i;
+        testFindRoot(&alefeld5, 0, 1);
+    }
+    foreach(i; nvals_6) {
+        n=i;
+        testFindRoot(&alefeld6, 0, 1);
+    }
+    foreach(i; nvals_7) {
+        n=i;
+        testFindRoot(&alefeld7, 0.01L, 1);
+    }   
+    real worstcase(real x) { ++numCalls;
+        return x<0.3*real.max? -0.999e-3 : 1.0;
+    }
+    testFindRoot(&worstcase, -real.max, real.max);
+       
+/*   
+   int grandtotal=0;
+   foreach(calls; alefeldSums) {
+       grandtotal+=calls;
+   }
+   grandtotal-=2*numProblems;
+   printf("\nALEFELD TOTAL = %d avg = %f (alefeld avg=19.3 for double)\n", 
+   grandtotal, (1.0*grandtotal)/numProblems);
+   powercalls -= 2*powerProblems;
+   printf("POWER TOTAL = %d avg = %f ", powercalls, 
+        (1.0*powercalls)/powerProblems);
+*/        
 }
 
