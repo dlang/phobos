182bf526abc185e50fe8d864f6fa12712d37b2af
Walter Bright <walter@digitalmars.com>
2008-06-30 06:53:47 +0000

    added bugzilla issues

--- std/zip.d
+++ std/zip.d
@@ -10,6 +10,10 @@
  *	$(LI Only Zip version 20 formats are supported.)
  *	$(LI Only supports compression modes 0 (no compression) and 8 (deflate).)
  *	$(LI Does not support encryption.)
+ *	$(LI $(BUGZILLA 592))
+ *	$(LI $(BUGZILLA 1832))
+ *	$(LI $(BUGZILLA 2137))
+ *	$(LI $(BUGZILLA 2138))
  *	)
  *
  * Macros:
