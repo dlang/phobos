7de36d8138601d3ecec6f08b9f68268a686cf13c
Sean Kelly <sean@invisibleduck.org>
2009-03-25 14:53:41 +0000

    Posix targets should now build correctly against the core.stdc chages.

--- std/mmfile.d
+++ std/mmfile.d
@@ -34,6 +34,7 @@ module std.mmfile;
 private import std.file;
 private import std.c.stdio;
 private import std.c.stdlib;
+private import core.stdc.errno;
 private import std.path;
 private import std.string;
 
