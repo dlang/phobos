058fb734e3ee94ec8eea75c2937b4e2ac0a43dd3
Andrei Alexandrescu <andrei@erdani.com>
2009-04-06 19:46:45 +0000

    * Added declarations for struct_stat64, stat64, and fstat64.

--- std/c/linux/linux.d
+++ std/c/linux/linux.d
@@ -10,6 +10,33 @@ module std.c.linux.linux;
 
 public import std.c.linux.linuxextern;
 public import std.c.linux.pthread;
+
+struct struct_stat64	// distinguish it from the stat() function
+{
+    ulong st_dev;	/// device
+    uint __pad1;
+    uint st_ino;	/// file serial number
+    uint st_mode;	/// file mode
+    uint st_nlink;	/// link count
+    uint st_uid;	/// user ID of file's owner
+    uint st_gid;	/// user ID of group's owner
+    ulong st_rdev;	/// if device then device number
+    uint __pad2;
+    align(4) ulong st_size;
+    int st_blksize;	/// optimal I/O block size
+    ulong st_blocks;	/// number of allocated 512 byte blocks
+    int st_atime;
+    uint st_atimensec;
+    int st_mtime;
+    uint st_mtimensec;
+    int st_ctime;
+    uint st_ctimensec;
+
+    ulong st_ino64;
+}
+int fstat64(int, struct_stat64*);
+int fstat64(in char*, struct_stat64*);
+
 public import core.sys.posix.dirent;
 public import core.sys.posix.dlfcn;
 public import core.sys.posix.fcntl;
