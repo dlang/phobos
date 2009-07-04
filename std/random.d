f19d95abd41470e71892061bfd8e9e1637645052
Andrei Alexandrescu <andrei@erdani.com>
2009-07-04 07:04:29 +0000

    added randomSample.

--- std/random.d
+++ std/random.d
@@ -21,7 +21,7 @@ generator it considers the most fit for the target environment.
 Example:
 
 ----
-// Generate a uniformly-distributed integer in the range [0, 15)
+// Generate a uniformly-distributed integer in the range [0, 14]
 auto i = uniform(0, 15);
 // Generate a uniformly-distributed real in the range [0, 100$(RPAREN)
 // using a specific random generator
@@ -557,7 +557,8 @@ if (is(CommonType!(T1, UniformRandomNumberGenerator) == void) &&
         {
             static if (_b.min == 0)
             {
-                if (b == 0) {
+                if (b == 0)
+                {
                     // writeln("Invalid distribution range: "
                     //         ~ boundaries[0] ~ to!(string)(a)
                     //         ~ ", " ~ to!(string)(b) ~ boundaries[1]);
@@ -593,7 +594,7 @@ if (is(CommonType!(T1, UniformRandomNumberGenerator) == void) &&
             urng.popFront;
         }
         while (r > myRange);
-        return _a + r;
+        return cast(typeof(return)) (_a + r);
     }
     else
     {
@@ -764,10 +765,11 @@ struct RandomCover(Range, Random)
         popFront;
     }
 
-    auto opSlice()
-    {
-        return this;
-    }
+    static if (hasLength!Range)
+        size_t length()
+        {
+            return (1 + _input.length) - _alreadyChosen;
+        }
 
     ref ElementType!(Range) front()
     {
@@ -826,6 +828,144 @@ unittest
     }
     sort(b);
     assert(a == b, text(b));
+}
+
+// RandomSample
+/**
+Selects a random subsample out of $(D r), containing exactly $(D n)
+elements. The order of elements is the same as in the original
+range. The total length of $(D r) must be known. If $(D total) is
+passed in, the total number of sample is considered to be $(D
+total). Otherwise, $(D RandomSample) uses $(D r.length).
+
+If the number of elements is not exactly $(D total), $(D
+RandomSample) throws an exception. This is because $(D total) is
+essential to computing the probability of selecting elements in the
+range.
+
+Example:
+----
+int[] a = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+// Print 5 random elements picked off from r
+foreach (e; randomSample(n, 5))
+{
+    writeln(e);
+}
+----
+ */
+struct RandomSample(R)
+{
+    private size_t _available, _toSelect;
+    private R _input;
+    private size_t _index;
+    private enum bool byRef = is(typeof(&(R.init.front())));
+
+/**
+Constructor.
+*/
+    static if (hasLength!R)
+        this(R input, size_t howMany)
+        {
+            this(input, howMany, input.length);
+        }
+
+/// Ditto
+    this(R input, size_t howMany, size_t total)
+    {
+        _input = input;
+        _available = total;
+        _toSelect = howMany;
+        enforce(_toSelect <= _available);
+        // we should skip some elements initially so we don't always
+        // start with the first
+        prime;
+    }
+
+/**
+   Range primitives.
+*/
+    bool empty() const
+    {
+        return _toSelect == 0;
+    }
+
+    mixin((byRef ? "ref " : "")~
+            q{ElementType!R front()
+                {
+                    assert(!empty);
+                    return _input.front;
+                }});
+
+/// Ditto
+    void popFront()
+    {
+        _input.popFront;
+        --_available;
+        --_toSelect;
+        ++_index;
+        prime;
+    }
+
+/// Ditto
+    size_t length()
+    {
+        return _toSelect;
+    }
+
+/**
+Returns the index of the visited record.
+ */
+    size_t index()
+    {
+        return _index;
+    }
+
+    private void prime()
+    {
+        if (empty) return;
+        assert(_available && _available >= _toSelect);
+        for (;;)
+        {
+            auto r = uniform(0, _available);
+            if (r < _toSelect)
+            {
+                // chosen!
+                return;
+            }
+            // not chosen, retry
+            assert(!_input.empty);
+            _input.popFront;
+            ++_index;
+            --_available;
+            assert(_available > 0);
+        }
+    }
+}
+
+/// Ditto
+RandomSample!R randomSample(R)(R r, size_t n, size_t total)
+{
+    return typeof(return)(r, n, total);
+}
+
+/// Ditto
+RandomSample!R randomSample(R)(R r, size_t n) //if (hasLength!R) // @@@BUG@@@
+{
+    return typeof(return)(r, n, r.length);
+}
+
+unittest
+{
+    int[] a = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+    //int[] a = [ 0, 1, 2 ];
+    assert(randomSample(a, 5).length == 5);
+    uint i;
+    foreach (e; randomSample(randomCover(a, rndGen), 5))
+    {
+        ++i;
+        //writeln(e);
+    }
+    assert(i == 5);
 }
 
 //__EOF__
