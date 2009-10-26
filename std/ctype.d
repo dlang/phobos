b91c592ac8d391582b02cef059f17db5699a4a4f
Andrei Alexandrescu <andrei@erdani.com>
2009-10-26 22:41:15 +0000

    Add pure to isalnum, isalpha, iscntrl, isdigit, islower, ispunct, isspace, isxdigit, isgraph, isprint, isascii, toupper

--- std/ctype.d
+++ std/ctype.d
@@ -23,38 +23,38 @@ module std.ctype;
 /**
  * Returns !=0 if c is a letter in the range (0..9, a..z, A..Z).
  */
-int isalnum(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_ALP|_DIG) : 0; }
+pure int isalnum(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_ALP|_DIG) : 0; }
 
 /**
  * Returns !=0 if c is an ascii upper or lower case letter.
  */
-int isalpha(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_ALP)      : 0; }
+pure int isalpha(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_ALP)      : 0; }
 
 /**
  * Returns !=0 if c is a control character.
  */
-int iscntrl(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_CTL)      : 0; }
+pure int iscntrl(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_CTL)      : 0; }
 
 /**
  * Returns !=0 if c is a digit.
  */
-int isdigit(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_DIG)      : 0; }
+pure int isdigit(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_DIG)      : 0; }
 
 /**
  * Returns !=0 if c is lower case ascii letter.
  */
-int islower(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_LC)       : 0; }
+pure int islower(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_LC)       : 0; }
 
 /**
  * Returns !=0 if c is a punctuation character.
  */
-int ispunct(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_PNC)      : 0; }
+pure int ispunct(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_PNC)      : 0; }
 
 /**
  * Returns !=0 if c is a space, tab, vertical tab, form feed,
  * carriage return, or linefeed.
  */
-int isspace(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_SPC)      : 0; }
+pure int isspace(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_SPC)      : 0; }
 
 /**
  * Returns !=0 if c is an upper case ascii character.
@@ -64,22 +64,22 @@ pure int isupper(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_UC)       : 0; }
 /**
  * Returns !=0 if c is a hex digit (0..9, a..f, A..F).
  */
-int isxdigit(dchar c) { return (c <= 0x7F) ? _ctype[c] & (_HEX)      : 0; }
+pure int isxdigit(dchar c) { return (c <= 0x7F) ? _ctype[c] & (_HEX)      : 0; }
 
 /**
  * Returns !=0 if c is a printing character except for the space character.
  */
-int isgraph(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_ALP|_DIG|_PNC) : 0; }
+pure int isgraph(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_ALP|_DIG|_PNC) : 0; }
 
 /**
  * Returns !=0 if c is a printing character including the space character.
  */
-int isprint(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_ALP|_DIG|_PNC|_BLK) : 0; }
+pure int isprint(dchar c)  { return (c <= 0x7F) ? _ctype[c] & (_ALP|_DIG|_PNC|_BLK) : 0; }
 
 /**
  * Returns !=0 if c is in the ascii character set, i.e. in the range 0..0x7F.
  */
-int isascii(dchar c)  { return c <= 0x7F; }
+pure int isascii(dchar c)  { return c <= 0x7F; }
 
 
 /**
@@ -101,7 +101,7 @@ pure dchar tolower(dchar c)
  * If c is a lower case ascii character,
  * return the upper case equivalent, otherwise return c.
  */
-dchar toupper(dchar c)
+pure dchar toupper(dchar c)
     out (result)
     {
         assert(!islower(result));
