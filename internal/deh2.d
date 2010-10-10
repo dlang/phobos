3c9abb696cea5c9795e95e8df5083a36c1042dbe
Walter Bright <walter@digitalmars.com>
2010-10-10 20:50:04 +0000

    update license to Boost

--- internal/deh2.d
+++ internal/deh2.d
@@ -1,25 +1,14 @@
-/*
- *  Copyright (C) 1999-2010 by Digital Mars
- *  http://www.digitalmars.com
- *  Written by Walter Bright
+/**
+ * Implementation of exception handling support routines for Posix.
  *
- *  This software is provided 'as-is', without any express or implied
- *  warranty. In no event will the authors be held liable for any damages
- *  arising from the use of this software.
+ * Copyright: Copyright Digital Mars 2000 - 2010.
+ * License:   <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>.
+ * Authors:   Walter Bright
  *
- *  Permission is granted to anyone to use this software for any purpose,
- *  including commercial applications, and to alter it and redistribute it
- *  freely, in both source and binary form, subject to the following
- *  restrictions:
- *
- *  o  The origin of this software must not be misrepresented; you must not
- *     claim that you wrote the original software. If you use this software
- *     in a product, an acknowledgment in the product documentation would be
- *     appreciated but is not required.
- *  o  Altered source versions must be plainly marked as such, and must not
- *     be misrepresented as being the original software.
- *  o  This notice may not be removed or altered from any source
- *     distribution.
+ *          Copyright Digital Mars 2000 - 2009.
+ * Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
  */
 
 // Exception handling support for linux
@@ -361,4 +350,3 @@ extern (Windows) void _d_throw(Object *h)
         }
     }
 }
-
