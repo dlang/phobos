8f7b9d0d1322901cbaa6f8038747188ef89bbe18
KennyTM~ <kennytm@gmail.com>
2011-06-21 16:42:44 +0800

    Issue 5873 - Cannot call iota() on long with step

--- std/range.d
+++ std/range.d
@@ -3897,7 +3897,7 @@ if ((isIntegral!(CommonType!(B, E)) || isPointer!(CommonType!(B, E)))
             ret.pastLast -= (this.length - upper) * step;
             return ret;
         }
-        @property size_t length() const
+        @property typeof(unsigned((pastLast - current) / step)) length() const
         {
             return unsigned((pastLast - current) / step);
         }
@@ -4133,6 +4133,11 @@ unittest
     // iota of longs
     auto rl = iota(5_000_000L);
     assert(rl.length == 5_000_000L);
+
+    // iota of longs with steps
+    auto iota_of_longs_with_steps = iota(50L, 101L, 10);
+    assert(iota_of_longs_with_steps.length == 6);
+    assert(equal(iota_of_longs_with_steps, [50L, 60L, 70L, 80L, 90L, 100L]));
 }
 
 unittest
