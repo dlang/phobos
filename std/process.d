03f67f27b729a93769bd2818026e5c732e64a71a
jmdavis <jmdavisProg@gmx.com>
2011-07-22 08:59:36 -0700

    Added function to std.process for opening default browser with a given URL.

--- std/process.d
+++ std/process.d
@@ -724,3 +724,97 @@ unittest
         assert (v == environment[n]);
     }
 }
+
+
+version (Windows)
+{
+    import core.sys.windows.windows;
+
+    extern (Windows)
+    HINSTANCE ShellExecuteA(HWND hwnd, LPCSTR lpOperation, LPCSTR lpFile, LPCSTR lpParameters, LPCSTR lpDirectory, INT nShowCmd);
+
+
+    pragma(lib,"shell32.lib");
+
+    /****************************************
+     * Start up the browser and set it to viewing the page at url.
+     */
+    void browse(string url)
+    {
+        ShellExecuteA(null, "open", toStringz(url), null, null, SW_SHOWNORMAL);
+    }
+}
+else version (OSX)
+{
+    import core.stdc.stdio;
+    import core.stdc.string;
+    import core.sys.posix.unistd;
+
+    void browse(string url)
+    {
+        const(char)*[5] args;
+
+        const(char)* browser = core.stdc.stdlib.getenv("BROWSER");
+        if (browser)
+        {   browser = strdup(browser);
+            args[0] = browser;
+            args[1] = toStringz(url);
+            args[2] = null;
+        }
+        else
+        {
+            //browser = "/Applications/Safari.app/Contents/MacOS/Safari";
+            args[0] = "open".ptr;
+            args[1] = "-a".ptr;
+            args[2] = "/Applications/Safari.app".ptr;
+            args[3] = toStringz(url);
+            args[4] = null;
+        }
+
+        auto childpid = fork();
+        if (childpid == 0)
+        {
+            core.sys.posix.unistd.execvp(args[0], cast(char**)args.ptr);
+            perror(args[0]);                // failed to execute
+            return;
+        }
+        if (browser)
+            free(cast(void*)browser);
+    }
+}
+else version (Posix)
+{
+    import core.stdc.stdio;
+    import core.stdc.string;
+    import core.sys.posix.unistd;
+
+    void browse(string url)
+    {
+        const(char)*[3] args;
+
+        const(char)* browser = core.stdc.stdlib.getenv("BROWSER");
+        if (browser)
+        {   browser = strdup(browser);
+            args[0] = browser;
+        }
+        else
+            args[0] = "x-www-browser".ptr;
+
+        args[1] = toStringz(url);
+        args[2] = null;
+
+        auto childpid = fork();
+        if (childpid == 0)
+        {
+            core.sys.posix.unistd.execvp(args[0], cast(char**)args.ptr);
+            perror(args[0]);                // failed to execute
+            return;
+        }
+        if (browser)
+            free(cast(void*)browser);
+    }
+}
+else
+    static assert(0, "os not supported");
+
+
