eb618ccd67bd105464e8b2423cac2f4a46e9b14a
k-hara <k.hara.pg@gmail.com>
2011-08-10 16:15:02 +0900

    std.traits.isIntegral never match any char types.

--- std/conv.d
+++ std/conv.d
@@ -1750,7 +1750,7 @@ assert(test == "");
  */
 
 Target parse(Target, Source)(ref Source s)
-    if (isSomeChar!(ElementType!Source) && isIntegral!Target && !isSomeChar!Target)
+    if (isSomeChar!(ElementType!Source) && isIntegral!Target)
 {
     static if (Target.sizeof < int.sizeof)
     {
