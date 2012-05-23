dlang/phobos

commit 4ee4efe2b03d4d3221a96e06c6b10fd14350390e

fix Issue 7796 - std.typecons.Unique is using writeln without importing std.stdio

std/typecons.d
@@ -88,7 +88,7 @@ public:
     */
     this(RefT p)
     {
-        writeln("Unique constructor with rvalue");
+        version(unittest) writeln("Unique constructor with rvalue");
         _p = p;
     }
     /**
@@ -99,7 +99,7 @@ public:
     this(ref RefT p)
     {
         _p = p;
-        writeln("Unique constructor nulling source");
+        version(unittest) writeln("Unique constructor nulling source");
         p = null;
         assert(p is null);
     }
@@ -131,7 +131,7 @@ public:
 
     ~this()
     {
-        writeln("Unique destructor of ", (_p is null)? null: _p);
+        version(unittest) writeln("Unique destructor of ", (_p is null)? null: _p);
         delete _p;
         _p = null;
     }
@@ -142,10 +142,10 @@ public:
     /** Returns a unique rvalue. Nullifies the current contents */
     Unique release()
     {
-        writeln("Release");
+        version(unittest) writeln("Release");
         auto u = Unique(_p);
         assert(_p is null);
-        writeln("return from Release");
+        version(unittest) writeln("return from Release");
         return u;
     }
     /** Forwards member access to contents */

commit 61f991ba515c5cb48b636a8c803ebbb5804e8508

Issue 7796 - std.typecons.Unique is using writeln without importing std.stdio.
With Unique being a template, commit 4ee4efe (pull request #601) caused the messages to be printed for unittest builds of client code as well.

std/typecons.d
@@ -46,7 +46,7 @@ import core.memory, core.stdc.stdlib;
 import std.algorithm, std.array, std.conv, std.exception, std.format,
     std.metastrings, std.traits, std.typetuple, std.range;
 
-version(unittest) import core.vararg, std.stdio;
+debug(Unique) import std.stdio;
 
 /**
 Encapsulates unique ownership of a resource.  Resource of type T is
@@ -57,7 +57,6 @@ class object, in which case Unique behaves polymorphically too.
 
 Example:
 */
-
 struct Unique(T)
 {
 static if (is(T:Object))
@@ -88,7 +87,7 @@ public:
     */
     this(RefT p)
     {
-        version(unittest) writeln("Unique constructor with rvalue");
+        debug(Unique) writeln("Unique constructor with rvalue");
         _p = p;
     }
     /**
@@ -99,7 +98,7 @@ public:
     this(ref RefT p)
     {
         _p = p;
-        version(unittest) writeln("Unique constructor nulling source");
+        debug(Unique) writeln("Unique constructor nulling source");
         p = null;
         assert(p is null);
     }
@@ -131,7 +130,7 @@ public:
 
     ~this()
     {
-        version(unittest) writeln("Unique destructor of ", (_p is null)? null: _p);
+        debug(Unique) writeln("Unique destructor of ", (_p is null)? null: _p);
         delete _p;
         _p = null;
     }
@@ -142,10 +141,10 @@ public:
     /** Returns a unique rvalue. Nullifies the current contents */
     Unique release()
     {
-        version(unittest) writeln("Release");
+        debug(Unique) writeln("Release");
         auto u = Unique(_p);
         assert(_p is null);
-        version(unittest) writeln("return from Release");
+        debug(Unique) writeln("return from Release");
         return u;
     }
     /** Forwards member access to contents */
@@ -2034,6 +2033,7 @@ private static:
 }
 
 //debug = SHOW_GENERATED_CODE;
+version(unittest) import core.vararg;
 unittest
 {
     // no function to implement