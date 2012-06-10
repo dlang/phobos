3d01055d96158095d96acea6f2618d1db1a49a90
alexrp <alex@lycus.org>
2012-06-10 10:08:40 +0200

    Mark some std.stdio.File members pure/nothrow.

--- std/stdio.d
+++ std/stdio.d
@@ -343,7 +343,7 @@ opengroup.org/onlinepubs/007908799/xsh/_popen.html, _popen).
     }
 
 /** Returns $(D true) if the file is opened. */
-    @property bool isOpen() const
+    @property bool isOpen() const pure nothrow
     {
         return p !is null && p.handle;
     }
@@ -353,14 +353,14 @@ Returns $(D true) if the file is at end (see $(WEB
 cplusplus.com/reference/clibrary/cstdio/feof.html, feof)). The file
 must be opened, otherwise an exception is thrown.
  */
-    @property bool eof() const
+    @property bool eof() const pure
     {
         enforce(p && p.handle, "Calling eof() against an unopened file.");
         return .feof(cast(FILE*) p.handle) != 0;
     }
 
 /** Returns the name of the file, if any. */
-    @property string name() const
+    @property string name() const pure nothrow
     {
         return p.name;
     }
@@ -370,7 +370,7 @@ If the file is not opened, returns $(D false). Otherwise, returns
 $(WEB cplusplus.com/reference/clibrary/cstdio/ferror.html, ferror) for
 the file handle.
  */
-    @property bool error() const
+    @property bool error() const pure nothrow
     {
         return !p.handle || .ferror(cast(FILE*) p.handle);
     }
@@ -444,7 +444,7 @@ If the file is not opened, succeeds vacuously. Otherwise, returns
 $(WEB cplusplus.com/reference/clibrary/cstdio/_clearerr.html,
 _clearerr) for the file handle.
  */
-    void clearerr()
+    void clearerr() pure nothrow
     {
         p is null || p.handle is null ||
         .clearerr(p.handle);
@@ -919,7 +919,7 @@ File) never takes the initiative in closing the file. */
 /**
 Returns the $(D FILE*) corresponding to this object.
  */
-    FILE* getFP()
+    FILE* getFP() pure
     {
         enforce(p && p.handle,
                 "Attempting to call getFP() on an unopened file");
