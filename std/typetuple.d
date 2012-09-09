e6df118840fe686b60a2915c73182128f721a011
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-09-09 16:04:08 +0400

    Add std.typetuple for backward compatibility

---
+++ std/typetuple.d
@@ -0,0 +1,10 @@
+// Written in the D programming language.
+
+/**
+ * $(RED Warning: This module is renamed to $(D std.generictuple).)
+ */
+module std.typetuple;
+
+public import std.generictuple;
+
+alias GenericTuple TypeTuple;
