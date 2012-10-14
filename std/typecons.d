a723713475072ccf7c7e9a7a13bcfd13fcc1e43d
jmdavis <jmdavisProg@gmx.com>
2012-10-13 19:44:25 -0700

    Small tweak to Rebindable.
    It should use inout rather than duplicating the get function for mutable
    and const.

--- std/typecons.d
+++ std/typecons.d
@@ -910,11 +910,7 @@ template Rebindable(T) if (is(T == class) || is(T == interface) || isArray!(T))
                 opAssign(initializer);
             }
 
-            @property ref T get() pure nothrow
-            {
-                return original;
-            }
-            @property ref const(T) get() const pure nothrow
+            @property ref inout(T) get() inout pure nothrow
             {
                 return original;
             }
