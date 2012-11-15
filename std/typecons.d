ef71030310c712d49412bcc4581bfc6f207d4d4c
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-11-15 20:46:25 +0400

    Fix `scoped` unittests.
    Using `scoped` with nested class compiles but shouldn't as it results in undefined behavior.

--- std/typecons.d
+++ std/typecons.d
@@ -3191,7 +3191,7 @@ unittest // Issue 6580 testcase
 
 unittest // Original Issue 6580 testcase
 {
-    class C { int i; byte b; }
+    static class C { int i; byte b; }
 
     auto sa = [scoped!C(), scoped!C()];
     assert(cast(size_t)&sa[0].i % int.alignof == 0);
@@ -3200,7 +3200,7 @@ unittest // Original Issue 6580 testcase
 
 unittest
 {
-    class A { int x = 1; }
+    static class A { int x = 1; }
     auto a1 = scoped!A();
     assert(a1.x == 1);
     auto a2 = scoped!A();
@@ -3211,7 +3211,7 @@ unittest
 
 unittest
 {
-    class A { int x = 1; this() { x = 2; } }
+    static class A { int x = 1; this() { x = 2; } }
     auto a1 = scoped!A();
     assert(a1.x == 2);
     auto a2 = scoped!A();
@@ -3222,7 +3222,7 @@ unittest
 
 unittest
 {
-    class A { int x = 1; this(int y) { x = y; } ~this() {} }
+    static class A { int x = 1; this(int y) { x = y; } ~this() {} }
     auto a1 = scoped!A(5);
     assert(a1.x == 5);
     auto a2 = scoped!A(42);
@@ -3233,8 +3233,8 @@ unittest
 
 unittest
 {
-    class A { static bool dead; ~this() { dead = true; } }
-    class B : A { static bool dead; ~this() { dead = true; } }
+    static class A { static bool dead; ~this() { dead = true; } }
+    static class B : A { static bool dead; ~this() { dead = true; } }
     {
         auto b = scoped!B();
     }
@@ -3268,7 +3268,7 @@ unittest // Issue 8039 testcase
 unittest
 {
     // bug4500
-    class A
+    static class A
     {
         this() { a = this; }
         this(int i) { a = this; }
@@ -3346,7 +3346,7 @@ unittest
 
 unittest
 {
-    class C { this(ref int val) { assert(val == 3); ++val; } }
+    static class C { this(ref int val) { assert(val == 3); ++val; } }
 
     int val = 3;
     auto s = scoped!C(val);
