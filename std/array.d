87bd3315db559ecca11e3bf69ac0410a16da7d8b
k-hara <k.hara.pg@gmail.com>
2013-06-02 01:10:32 +0900

    fix Issue 10230 - Duplicated buttons for runnable examples

--- std/array.d
+++ std/array.d
@@ -25,14 +25,10 @@ $(D opApply) function $(D r).  Note that narrow strings are handled as
 a special case in an overload.
 
 Example:
-
-$(D_RUN_CODE
-$(ARGS
 ----
 auto a = array([1, 2, 3, 4, 5][]);
 assert(a == [ 1, 2, 3, 4, 5 ]);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
  */
 ForeachType!Range[] array(Range)(Range r)
 if (isIterable!Range && !isNarrowString!Range)
@@ -195,16 +191,12 @@ Returns a newly allocated associative array out of elements of the input range,
 which must be a range of tuples (Key, Value).
 
 Example:
-
-$(D_RUN_CODE
-$(ARGS
 ----
 auto a = assocArray(zip([0, 1, 2], ["a", "b", "c"]));
 assert(a == [0:"a", 1:"b", 2:"c"]);
 auto b = assocArray([ tuple("foo", "bar"), tuple("baz", "quux") ]);
 assert(b == ["foo":"bar", "baz":"quux"]);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
  */
 
 auto assocArray(Range)(Range r)
@@ -277,8 +269,6 @@ array.  In this case sizes may be specified for any number of dimensions from 1
 to the number in $(D T).
 
 Examples:
-$(D_RUN_CODE
-$(ARGS
 ---
 double[] arr = uninitializedArray!(double[])(100);
 assert(arr.length == 100);
@@ -287,7 +277,6 @@ double[][] matrix = uninitializedArray!(double[][])(42, 31);
 assert(matrix.length == 42);
 assert(matrix[0].length == 31);
 ---
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
 */
 auto uninitializedArray(T, I...)(I sizes)
 if(allSatisfy!(isIntegral, I))
@@ -380,14 +369,11 @@ the first argument using the dot notation, $(D array.empty) is
 equivalent to $(D empty(array)).
 
 Example:
-$(D_RUN_CODE
-$(ARGS
 ----
 auto a = [ 1, 2, 3 ];
 assert(!a.empty);
 assert(a[3 .. $].empty);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
  */
 
 @property bool empty(T)(in T[] a) @safe pure nothrow
@@ -410,14 +396,11 @@ equivalent to $(D save(array)). The function does not duplicate the
 content of the array, it simply returns its argument.
 
 Example:
-$(D_RUN_CODE
-$(ARGS
 ----
 auto a = [ 1, 2, 3 ];
 auto b = a.save;
 assert(b is a);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
  */
 
 @property T[] save(T)(T[] a) @safe pure nothrow
@@ -434,14 +417,11 @@ $(D popFront) automaticaly advances to the next $(GLOSSARY code
 point).
 
 Example:
-$(D_RUN_CODE
-$(ARGS
 ----
 int[] a = [ 1, 2, 3 ];
 a.popFront();
 assert(a == [ 2, 3 ]);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
 */
 
 void popFront(T)(ref T[] a)
@@ -538,14 +518,11 @@ popFront) automaticaly eliminates the last $(GLOSSARY code point).
 
 
 Example:
-$(D_RUN_CODE
-$(ARGS
 ----
 int[] a = [ 1, 2, 3 ];
 a.popBack();
 assert(a == [ 1, 2 ]);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
 */
 
 void popBack(T)(ref T[] a)
@@ -610,13 +587,10 @@ dchar).
 
 
 Example:
-$(D_RUN_CODE
-$(ARGS
 ----
 int[] a = [ 1, 2, 3 ];
 assert(a.front == 1);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
 */
 @property ref T front(T)(T[] a)
 if (!isNarrowString!(T[]) && !is(T[] == void[]))
@@ -655,13 +629,10 @@ back) automaticaly returns the last $(GLOSSARY code point) as a $(D
 dchar).
 
 Example:
-$(D_RUN_CODE
-$(ARGS
 ----
 int[] a = [ 1, 2, 3 ];
 assert(a.back == 3);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
 */
 @property ref T back(T)(T[] a) if (!isNarrowString!(T[]))
 {
@@ -700,8 +671,6 @@ values referred by them. If $(D r1) and $(D r2) have an overlapping
 slice, returns that slice. Otherwise, returns the null slice.
 
 Example:
-$(D_RUN_CODE
-$(ARGS
 ----
 int[] a = [ 10, 11, 12, 13, 14 ];
 int[] b = a[1 .. 3];
@@ -710,7 +679,6 @@ b = b.dup;
 // overlap disappears even though the content is the same
 assert(overlap(a, b).empty);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
 */
 inout(T)[] overlap(T)(inout(T)[] r1, inout(T)[] r2) @trusted pure nothrow
 {
@@ -781,15 +749,12 @@ it's commented out.
     must be an input range or a single item) inserted at position $(D pos).
 
     Examples:
-$(D_RUN_CODE
-$(ARGS
---------------------
-int[] a = [ 1, 2, 3, 4 ];
-auto b = a.insert(2, [ 1, 2 ]);
-assert(a == [ 1, 2, 3, 4 ]);
-assert(b == [ 1, 2, 1, 2, 3, 4 ]);
---------------------
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
+    --------------------
+    int[] a = [ 1, 2, 3, 4 ];
+    auto b = a.insert(2, [ 1, 2 ]);
+    assert(a == [ 1, 2, 3, 4 ]);
+    assert(b == [ 1, 2, 1, 2, 3, 4 ]);
+    --------------------
  +/
 T[] insert(T, Range)(T[] array, size_t pos, Range stuff)
     if(isInputRange!Range &&
@@ -901,17 +866,14 @@ private void copyBackwards(T)(T[] src, T[] dest)
     Inserts $(D stuff) (which must be an input range or any number of
     implicitly convertible items) in $(D array) at position $(D pos).
 
-Example:
-$(D_RUN_CODE
-$(ARGS
----
-int[] a = [ 1, 2, 3, 4 ];
-a.insertInPlace(2, [ 1, 2 ]);
-assert(a == [ 1, 2, 1, 2, 3, 4 ]);
-a.insertInPlace(3, 10u, 11);
-assert(a == [ 1, 2, 1, 10, 11, 2, 3, 4]);
----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
+    Example:
+    ---
+    int[] a = [ 1, 2, 3, 4 ];
+    a.insertInPlace(2, [ 1, 2 ]);
+    assert(a == [ 1, 2, 1, 2, 3, 4 ]);
+    a.insertInPlace(3, 10u, 11);
+    assert(a == [ 1, 2, 1, 10, 11, 2, 3, 4]);
+    ---
  +/
 void insertInPlace(T, U...)(ref T[] array, size_t pos, U stuff)
     if(!isSomeString!(T[])
@@ -1379,13 +1341,10 @@ unittest
 Splits a string by whitespace.
 
 Example:
-$(D_RUN_CODE
-$(ARGS
 ----
 auto a = " a     bcd   ef gh ";
 assert(equal(splitter(a), ["", "a", "bcd", "ef", "gh"][]));
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array, std.algorithm: equal;))
  */
 auto splitter(C)(C[] s)
     if(isSomeString!(C[]))
@@ -1478,17 +1437,14 @@ unittest
    Concatenates all of the ranges in $(D ror) together into one array using
    $(D sep) as the separator if present.
 
-Examples:
-$(D_RUN_CODE
-$(ARGS
---------------------
-assert(join(["hello", "silly", "world"], " ") == "hello silly world");
-assert(join(["hello", "silly", "world"]) == "hellosillyworld");
+    Examples:
+    --------------------
+    assert(join(["hello", "silly", "world"], " ") == "hello silly world");
+    assert(join(["hello", "silly", "world"]) == "hellosillyworld");
 
-assert(join([[1, 2, 3], [4, 5]], [72, 73]) == [1, 2, 3, 72, 73, 4, 5]);
-assert(join([[1, 2, 3], [4, 5]]) == [1, 2, 3, 4, 5]);
---------------------
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
+    assert(join([[1, 2, 3], [4, 5]], [72, 73]) == [1, 2, 3, 72, 73, 4, 5]);
+    assert(join([[1, 2, 3], [4, 5]]) == [1, 2, 3, 4, 5]);
+    --------------------
   +/
 ElementEncodingType!(ElementType!RoR)[] join(RoR, R)(RoR ror, R sep)
     if(isInputRange!RoR &&
@@ -1780,16 +1736,13 @@ until then, it's commented out.
     (inclusive) to $(D to) (exclusive) with the range $(D stuff). Returns a new
     array without changing the contents of $(D subject).
 
-Examples:
-$(D_RUN_CODE
-$(ARGS
---------------------
-auto a = [ 1, 2, 3, 4 ];
-auto b = a.replace(1, 3, [ 9, 9, 9 ]);
-assert(a == [ 1, 2, 3, 4 ]);
-assert(b == [ 1, 9, 9, 9, 4 ]);
---------------------
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
+    Examples:
+    --------------------
+    auto a = [ 1, 2, 3, 4 ];
+    auto b = a.replace(1, 3, [ 9, 9, 9 ]);
+    assert(a == [ 1, 2, 3, 4 ]);
+    assert(b == [ 1, 9, 9, 9, 4 ]);
+    --------------------
  +/
 T[] replace(T, Range)(T[] subject, size_t from, size_t to, Range stuff)
     if(isInputRange!Range &&
@@ -1879,15 +1832,12 @@ unittest
     (inclusive) to $(D to) (exclusive) with the range $(D stuff). Expands or
     shrinks the array as needed.
 
-Example:
-$(D_RUN_CODE
-$(ARGS
----
-int[] a = [ 1, 2, 3, 4 ];
-a.replaceInPlace(1, 3, [ 9, 9, 9 ]);
-assert(a == [ 1, 9, 9, 9, 4 ]);
----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
+    Example:
+    ---
+    int[] a = [ 1, 2, 3, 4 ];
+    a.replaceInPlace(1, 3, [ 9, 9, 9 ]);
+    assert(a == [ 1, 9, 9, 9, 4 ]);
+    ---
  +/
 void replaceInPlace(T, Range)(ref T[] array, size_t from, size_t to, Range stuff)
     if(isDynamicArray!Range &&
@@ -2109,8 +2059,6 @@ recommended over $(D a ~= data) when appending many elements because it is more
 efficient.
 
 Example:
-$(D_RUN_CODE
-$(ARGS
 ----
 auto app = appender!string();
 string b = "abcdefg";
@@ -2123,7 +2071,6 @@ app2.put(3);
 app2.put([ 4, 5, 6 ]);
 assert(app2.data == [ 1, 2, 3, 4, 5, 6 ]);
 ----
-), $(ARGS), $(ARGS), $(ARGS import std.array;))
  */
 struct Appender(A : T[], T)
 {
