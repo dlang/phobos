3851d825ecfc117017640653d55eaa40e59acb47
Tomoya Tanjo <ttanjo@gmail.com>
2013-08-25 22:52:13 +0900

    Make std.string.removechars @safe and pure

--- std/string.d
+++ std/string.d
@@ -2583,7 +2583,7 @@ unittest
  * Return string that is s with all characters removed that match pattern.
  */
 
-S removechars(S)(S s, in S pattern) if (isSomeString!S)
+S removechars(S)(S s, in S pattern) @safe pure if (isSomeString!S)
 {
     Unqual!(typeof(s[0]))[] r;
     bool changed = false;
@@ -2604,7 +2604,8 @@ S removechars(S)(S s, in S pattern) if (isSomeString!S)
             std.utf.encode(r, c);
         }
     }
-    return (changed ? cast(S) r : s);
+    auto trustedAssumeUnique(typeof(r) a)@trusted{ return assumeUnique(a); }
+    return (changed ? trustedAssumeUnique(r) : s);
 }
 
 unittest
