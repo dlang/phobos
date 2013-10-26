f2f11867dae630ba950090648ed7c9a3bda9471b
Walter Bright <walter@walterbright.com>
2013-10-26 13:32:12 -0700

    Merge pull request #1667 from Kapps/fix11361
    Fix issue 11361 - Variant fails to compile with functions containing const arguments.

--- std/variant.d
+++ std/variant.d
@@ -493,7 +493,43 @@ private:
             }
 
         case OpID.apply:
-            assert(0);
+            static if (!isFunctionPointer!A && !isDelegate!A)
+            {
+                enforce(0, text("Cannot apply `()' to a value of type `",
+                                A.stringof, "'."));
+            }
+            else
+            {
+                alias ParamTypes = ParameterTypeTuple!A;
+                auto p = cast(VariantN*) parm;
+                auto argCount = p.get!size_t();
+                // To assign the tuple we need to use the unqualified version,
+                // otherwise we run into issues such as with const values.
+                // We still get the actual type from the Variant though
+                // to ensure that we retain const correctness.
+                Tuple!(staticMap!(Unqual, ParamTypes)) t;
+                enforce(t.length == argCount,
+                        text("Argument count mismatch: ",
+                             A.stringof, " expects ", t.length,
+                             " argument(s), not ", argCount, "."));
+                auto variantArgs = p[1 .. argCount + 1];
+                foreach (i, T; ParamTypes)
+                {
+                    t[i] = cast()variantArgs[i].get!T();
+                }
+
+                auto args = cast(Tuple!(ParamTypes))t;
+                static if(is(ReturnType!A == void))
+                {
+                    (*zis)(args.expand);
+                    *p = VariantN.init; // void returns uninitialized Variant.
+                }
+                else
+                {
+                    *p = (*zis)(args.expand);
+                }
+            }
+            break;
 
         default: assert(false);
         }
@@ -1558,6 +1594,36 @@ unittest
     // bug 7070
     Variant v;
     v = null;
+}
+
+// Const parameters with opCall, issue 11361.
+unittest
+{
+    static string t1(string c) {
+        return c ~ "a";
+    }
+
+    static const(char)[] t2(const(char)[] p) {
+        return p ~ "b";
+    }
+
+    static char[] t3(int p) {
+        return p.text.dup;
+    }
+
+    Variant v1 = &t1;
+    Variant v2 = &t2;
+    Variant v3 = &t3;
+
+    assert(v1("abc") == "abca");
+    assert(v1("abc").type == typeid(string));
+    assert(v2("abc") == "abcb");
+
+    assert(v2(cast(char[])("abc".dup)) == "abcb");
+    assert(v2("abc").type == typeid(const(char)[]));
+
+    assert(v3(4) == ['4']);
+    assert(v3(4).type == typeid(char[]));
 }
 
 // Ordering comparisons of incompatible types, e.g. issue 7990.
