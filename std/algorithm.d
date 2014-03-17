59ad77ad9fd898c3e1757548d6ff9e738b966609
monarchdodra <monarchdodra@gmail.com>
2014-03-17 20:31:27 +0100

    Improve emplaceRef for qualified construction

--- std/algorithm.d
+++ std/algorithm.d
@@ -790,7 +790,7 @@ template reduce(fun...) if (fun.length >= 1)
                         result = void;
                     foreach (i, T; result.Types)
                     {
-                        emplaceRef(result[i], seed);
+                        emplaceRef!T(result[i], seed);
                     }
                     r.popFront();
                     return reduce(result, r);
@@ -856,7 +856,7 @@ template reduce(fun...) if (fun.length >= 1)
 
                     foreach (i, T; result.Types)
                     {
-                        emplaceRef(result[i], elem);
+                        emplaceRef!T(result[i], elem);
                     }
                 }
             }
@@ -1407,7 +1407,7 @@ void uninitializedFill(Range, Value)(Range range, Value filler)
 
         // Must construct stuff by the book
         for (; !range.empty; range.popFront())
-            emplaceRef(range.front, filler);
+            emplaceRef!T(range.front, filler);
     }
     else
         // Doesn't matter whether fill is initialized or not
