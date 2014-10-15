6abb903e417a1c28c37c2a66b8262dcc905dd0d2
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-10-15 18:18:17 +0000

    std.windows.syserror: Fix msg constness of WindowsException constructor

--- std/windows/syserror.d
+++ std/windows/syserror.d
@@ -42,7 +42,7 @@ version (StdDdoc)
      */
     class WindowsException : OSException
     {
-        @disable this(int dummy);
+        this(DWORD code, in char[] str=null, string file = null, size_t line = 0) @trusted;
     }
 
     /++
@@ -118,7 +118,7 @@ class WindowsException : OSException
 {
     import core.sys.windows.windows;
 
-    this(DWORD code, string str=null, string file = null, size_t line = 0) @trusted
+    this(DWORD code, in char[] str=null, string file = null, size_t line = 0) @trusted
     {
         auto buf = appender!string();
 
