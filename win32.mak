dlang/phobos

commit b77b89dceae4b3c809d03f7c5e4ead0538e24f74

Windows Makefiles: Don't generate duplicate HTML files for Druntime modules

win32.mak
@@ -683,9 +683,6 @@ $(DOC)\std_format.html : $(STDDOC) std\format.d
 $(DOC)\std_functional.html : $(STDDOC) std\functional.d
 	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_functional.html $(STDDOC) std\functional.d
 
-$(DOC)\std_gc.html : $(STDDOC) $(DRUNTIME)\src\core\memory.d
-	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_gc.html $(STDDOC) $(DRUNTIME)\src\core\memory.d
-
 $(DOC)\std_getopt.html : $(STDDOC) std\getopt.d
 	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_getopt.html $(STDDOC) std\getopt.d
 
@@ -748,9 +745,6 @@ $(DOC)\std_string.html : $(STDDOC) std\string.d
 
 $(DOC)\std_system.html : $(STDDOC) std\system.d
 	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_system.html $(STDDOC) std\system.d
-
-$(DOC)\std_thread.html : $(STDDOC) $(DRUNTIME)\src\core\thread.d
-	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_thread.html $(STDDOC) -I$(DRUNTIME)\src $(DRUNTIME)\src\core\thread.d
 
 $(DOC)\std_traits.html : $(STDDOC) std\traits.d
 	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_traits.html $(STDDOC) std\traits.d

win64.mak
@@ -647,9 +647,6 @@ $(DOC)\std_format.html : $(STDDOC) std\format.d
 $(DOC)\std_functional.html : $(STDDOC) std\functional.d
 	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_functional.html $(STDDOC) std\functional.d
 
-$(DOC)\std_gc.html : $(STDDOC) $(DRUNTIME)\src\core\memory.d
-	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_gc.html $(STDDOC) $(DRUNTIME)\src\core\memory.d
-
 $(DOC)\std_getopt.html : $(STDDOC) std\getopt.d
 	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_getopt.html $(STDDOC) std\getopt.d
 
@@ -712,9 +709,6 @@ $(DOC)\std_string.html : $(STDDOC) std\string.d
 
 $(DOC)\std_system.html : $(STDDOC) std\system.d
 	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_system.html $(STDDOC) std\system.d
-
-$(DOC)\std_thread.html : $(STDDOC) $(DRUNTIME)\src\core\thread.d
-	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_thread.html $(STDDOC) -I$(DRUNTIME)\src $(DRUNTIME)\src\core\thread.d
 
 $(DOC)\std_traits.html : $(STDDOC) std\traits.d
 	$(DMD) -c -o- $(DDOCFLAGS) -Df$(DOC)\std_traits.html $(STDDOC) std\traits.d

commit d0b153d8d10c86be5081ef8d279028082fd7d7c8

Windows Makefile fixup
Fixes building documentation.
Broken in b77b89dceae4b3c809d03f7c5e4ead0538e24f74

win32.mak
@@ -337,7 +337,6 @@ DOCS=	$(DOC)\object.html \
 	$(DOC)\std_file.html \
 	$(DOC)\std_format.html \
 	$(DOC)\std_functional.html \
-	$(DOC)\std_gc.html \
 	$(DOC)\std_getopt.html \
 	$(DOC)\std_json.html \
 	$(DOC)\std_math.html \
@@ -361,7 +360,6 @@ DOCS=	$(DOC)\object.html \
 	$(DOC)\std_stream.html \
 	$(DOC)\std_string.html \
 	$(DOC)\std_system.html \
-	$(DOC)\std_thread.html \
 	$(DOC)\std_traits.html \
 	$(DOC)\std_typecons.html \
 	$(DOC)\std_typetuple.html \

win64.mak
@@ -354,7 +354,6 @@ DOCS=	$(DOC)\object.html \
 	$(DOC)\std_file.html \
 	$(DOC)\std_format.html \
 	$(DOC)\std_functional.html \
-	$(DOC)\std_gc.html \
 	$(DOC)\std_getopt.html \
 	$(DOC)\std_json.html \
 	$(DOC)\std_math.html \
@@ -378,7 +377,6 @@ DOCS=	$(DOC)\object.html \
 	$(DOC)\std_stream.html \
 	$(DOC)\std_string.html \
 	$(DOC)\std_system.html \
-	$(DOC)\std_thread.html \
 	$(DOC)\std_traits.html \
 	$(DOC)\std_typecons.html \
 	$(DOC)\std_typetuple.html \