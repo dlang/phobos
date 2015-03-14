0189a6334a57f310bd6fa0faab4530c4a1107395
Martin Nowak <code@dawg.eu>
2015-03-14 14:30:03 +0100

    Merge pull request #3050 from WalterBright/fix14253
    fix Issue 14253 - [REG2.067b3] std.traits.ParameterStorageClassTuple giv...

--- std/traits.d
+++ std/traits.d
@@ -187,12 +187,17 @@ private
             case 'J': stc2 = ParameterStorageClass.out_;  break;
             case 'K': stc2 = ParameterStorageClass.ref_;  break;
             case 'L': stc2 = ParameterStorageClass.lazy_; break;
+            case 'N': if (mstr.length >= 2 && mstr[1] == 'k')
+                        stc2 = ParameterStorageClass.return_;
+                      break;
             default : break;
         }
         if (stc2 != ParameterStorageClass.init)
         {
             pstc |= stc2;
             mstr  = mstr[1 .. $];
+            if (stc2 & ParameterStorageClass.return_)
+                mstr  = mstr[1 .. $];
         }
 
         return Demangle!uint(pstc, mstr);
@@ -209,14 +214,15 @@ private
             'd': FunctionAttribute.property,
             'e': FunctionAttribute.trusted,
             'f': FunctionAttribute.safe,
-            'i': FunctionAttribute.nogc
+            'i': FunctionAttribute.nogc,
+            'j': FunctionAttribute.return_
         ];
         uint atts = 0;
 
         // FuncAttrs --> FuncAttr | FuncAttr FuncAttrs
-        // FuncAttr  --> empty | Na | Nb | Nc | Nd | Ne | Nf
+        // FuncAttr  --> empty | Na | Nb | Nc | Nd | Ne | Nf | Ni | Nj
         // except 'Ng' == inout, because it is a qualifier of function type
-        while (mstr.length >= 2 && mstr[0] == 'N' && mstr[1] != 'g')
+        while (mstr.length >= 2 && mstr[0] == 'N' && mstr[1] != 'g' && mstr[1] != 'k')
         {
             if (FunctionAttribute att = LOOKUP_ATTRIBUTE[ mstr[1] ])
             {
@@ -443,6 +449,7 @@ version(unittest)
         }
 
         ref const(Inner[string]) func( ref Inner var1, lazy scope string var2 );
+        ref const(Inner[string]) retfunc( return ref Inner var1 );
         Inner inoutFunc(inout Inner) inout;
         shared(const(Inner[string])[]) data;
         const Inner delegate(double, string) @safe nothrow deleg;
@@ -562,8 +569,9 @@ private template fqnType(T,
     {
         alias PSC = ParameterStorageClass;
 
-        return format("%s%s%s%s",
+        return format("%s%s%s%s%s",
             psc & PSC.scope_ ? "scope " : "",
+            psc & PSC.return_ ? "return " : "",
             psc & PSC.out_ ? "out " : "",
             psc & PSC.ref_ ? "ref " : "",
             psc & PSC.lazy_ ? "lazy " : ""
@@ -624,13 +632,15 @@ private template fqnType(T,
         static if (attrs == FA.none)
             return "";
         else
-            return format("%s%s%s%s%s%s",
+            return format("%s%s%s%s%s%s%s%s",
                  attrs & FA.pure_ ? " pure" : "",
                  attrs & FA.nothrow_ ? " nothrow" : "",
                  attrs & FA.ref_ ? " ref" : "",
                  attrs & FA.property ? " @property" : "",
                  attrs & FA.trusted ? " @trusted" : "",
-                 attrs & FA.safe ? " @safe" : ""
+                 attrs & FA.safe ? " @safe" : "",
+                 attrs & FA.nogc ? " @nogc" : "",
+                 attrs & FA.return_ ? " return" : ""
             );
     }
 
@@ -785,6 +795,7 @@ unittest
 
         // Function types + function attributes
         static assert(fqn!(typeof(func)) == format("const(%s[string])(ref %s, scope lazy string) ref", inner_name, inner_name));
+        static assert(fqn!(typeof(retfunc)) == format("const(%s[string])(return %s) ref", inner_name, inner_name));
         static assert(fqn!(typeof(inoutFunc)) == format("inout(%s(inout(%s)))", inner_name, inner_name));
         static assert(fqn!(typeof(deleg)) == format("const(%s delegate(double, string) nothrow @safe)", inner_name));
         static assert(fqn!(typeof(inoutDeleg)) == "inout(int) delegate(inout(int)) inout");
@@ -947,11 +958,12 @@ enum ParameterStorageClass : uint
      * These flags can be bitwise OR-ed together to represent complex storage
      * class.
      */
-    none   = 0,
-    scope_ = 0b000_1,  /// ditto
-    out_   = 0b001_0,  /// ditto
-    ref_   = 0b010_0,  /// ditto
-    lazy_  = 0b100_0,  /// ditto
+    none    = 0,
+    scope_  = 0b000_1,  /// ditto
+    out_    = 0b001_0,  /// ditto
+    ref_    = 0b010_0,  /// ditto
+    lazy_   = 0b100_0,  /// ditto
+    return_ = 0b1000_0, /// ditto
 }
 
 /// ditto
@@ -1015,14 +1027,15 @@ unittest
     void noparam() {}
     static assert(ParameterStorageClassTuple!noparam.length == 0);
 
-    void test(scope int, ref int, out int, lazy int, int) { }
+    void test(scope int, ref int, out int, lazy int, int, return ref int) { }
     alias test_pstc = ParameterStorageClassTuple!test;
-    static assert(test_pstc.length == 5);
+    static assert(test_pstc.length == 6);
     static assert(test_pstc[0] == STC.scope_);
     static assert(test_pstc[1] == STC.ref_);
     static assert(test_pstc[2] == STC.out_);
     static assert(test_pstc[3] == STC.lazy_);
     static assert(test_pstc[4] == STC.none);
+    static assert(test_pstc[5] == STC.return_);
 
     interface Test
     {
@@ -1046,6 +1059,16 @@ unittest
     // Bugzilla 9317
     static inout(int) func(inout int param) { return param; }
     static assert(ParameterStorageClassTuple!(typeof(func))[0] == STC.none);
+}
+
+unittest
+{
+    // Bugzilla 14253
+    static struct Foo {
+        ref Foo opAssign(ref Foo rhs) return { return this; }
+    }
+
+    alias tup = ParameterStorageClassTuple!(__traits(getOverloads, Foo, "opAssign")[0]);
 }
 
 
@@ -1250,6 +1273,7 @@ enum FunctionAttribute : uint
     immutable_ = 1 << 9,  /// ditto
     inout_     = 1 << 10, /// ditto
     shared_    = 1 << 11, /// ditto
+    return_    = 1 << 12, /// ditto
 }
 
 /// ditto
@@ -1409,6 +1433,7 @@ private FunctionAttribute extractAttribFlags(Attribs...)()
             case "immutable": res |= immutable_; break;
             case "inout":     res |= inout_; break;
             case "shared":    res |= shared_; break;
+            case "return":    res |= return_; break;
             default: assert(0, attrib);
         }
     }
@@ -1928,6 +1953,8 @@ template SetFunctionAttributes(T, string linkage, uint attrs)
             result ~= " inout";
         static if (attrs & FunctionAttribute.shared_)
             result ~= " shared";
+        static if (attrs & FunctionAttribute.return_)
+            result ~= " return";
 
         result ~= " SetFunctionAttributes;";
         return result;
@@ -2000,7 +2027,7 @@ unittest
 
             // Add all known attributes, excluding conflicting ones.
             enum allAttrs = reduce!"a | b"([EnumMembers!FA])
-                & ~FA.safe & ~FA.property & ~FA.const_ & ~FA.immutable_ & ~FA.inout_ & ~FA.shared_ & ~FA.system;
+                & ~FA.safe & ~FA.property & ~FA.const_ & ~FA.immutable_ & ~FA.inout_ & ~FA.shared_ & ~FA.system & ~FA.return_;
 
             alias T2 = SetFunctionAttributes!(T1, functionLinkage!T, allAttrs);
             static assert(functionAttributes!T2 == allAttrs);
@@ -4348,7 +4375,7 @@ template isCovariantWith(F, G)
                     enum uprStc = UprPSTCs[i];
                     enum lwrStc = LwrPSTCs[i];
                     //
-                    enum wantExact = STC.out_ | STC.ref_ | STC.lazy_;
+                    enum wantExact = STC.out_ | STC.ref_ | STC.lazy_ | STC.return_;
                     enum ok =
                         ((uprStc & wantExact )  == (lwrStc & wantExact )) &&
                         ((uprStc & STC.scope_)  >= (lwrStc & STC.scope_)) &&
