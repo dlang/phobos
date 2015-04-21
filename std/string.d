aca2ed5168111d446f553129d09b9453d59445ae
Walter Bright <walter@walterbright.com>
2015-04-21 02:38:16 -0700

    remove pointless std.string.translateImplAscii()

--- std/string.d
+++ std/string.d
@@ -3622,7 +3622,14 @@ body
     }
 
     auto buffer = new char[count];
-    translateImplAscii(str, transTable, remTable, buffer, toRemove);
+
+    size_t i = 0;
+    foreach (char c; str)
+    {
+        if (!remTable[c])
+            buffer[i++] = transTable[c];
+    }
+
     return cast(C[])(buffer);
 }
 
@@ -3728,7 +3735,11 @@ body
     foreach (char c; toRemove)
         remTable[c] = true;
 
-    translateImplAscii(str, transTable, remTable, buffer, toRemove);
+    foreach (char c; str)
+    {
+        if (!remTable[c])
+            put(buffer, transTable[c]);
+    }
 }
 
 ///
@@ -3745,28 +3756,6 @@ body
     assert(buffer.data == "h5 rd");
 }
 
-private void translateImplAscii(C = immutable char, Buffer)(in char[] str,
-        in char[] transTable, ref bool[256] remTable, Buffer buffer,
-        in char[] toRemove = null) @trusted pure
-{
-    static if (isOutputRange!(Buffer, char))
-    {
-        foreach (char c; str)
-        {
-            if (!remTable[c])
-                put(buffer, transTable[c]);
-        }
-    }
-    else
-    {
-        size_t i = 0;
-        foreach (char c; str)
-        {
-            if (!remTable[c])
-                buffer[i++] = transTable[c];
-        }
-    }
-}
 
 /***********************************************
  * See if character c is in the pattern.
