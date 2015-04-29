b170d9369393e2df8c26522c49b4954f832c2cae
Dragos Carp <dragoscarp@gmail.com>
2015-04-29 11:01:54 +0200

    Fix some PHOBOSSRC broken links

--- std/internal/processinit.d
+++ std/internal/processinit.d
@@ -7,7 +7,7 @@
     Copyright: Copyright 2011 -
     License:   $(WEB www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
     Authors:   Jonathan M Davis and Kato Shoichi
-    Source:    $(PHOBOSSRC std/_datetime.d)
+    Source:    $(PHOBOSSRC std/internal/_processinit.d)
   +/
 module std.internal.processinit;
 
