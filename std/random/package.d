802c0f2acb3fb58e8d8e34600d21e552a390b258
Joseph Rushton Wakeling <joe@webdrake.net>
2015-06-05 19:35:44 +0200

    Move isUniformRNG and isSeedable to std.random.traits

--- std/random/package.d
+++ std/random/package.d
@@ -60,6 +60,7 @@ Distributed under the Boost Software License, Version 1.0.
 */
 module std.random;
 
+public import std.random.traits;
 
 import std.range.primitives;
 import std.traits;
@@ -119,133 +120,6 @@ version(unittest)
    email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
 */
 
-/**
- * Test if Rng is a random-number generator. The overload
- * taking a ElementType also makes sure that the Rng generates
- * values of that type.
- *
- * A random-number generator has at least the following features:
- * $(UL
- *   $(LI it's an InputRange)
- *   $(LI it has a 'bool isUniformRandom' field readable in CTFE)
- * )
- */
-template isUniformRNG(Rng, ElementType)
-{
-    enum bool isUniformRNG = isInputRange!Rng &&
-        is(typeof(Rng.front) == ElementType) &&
-        is(typeof(
-        {
-            static assert(Rng.isUniformRandom); //tag
-        }));
-}
-
-/**
- * ditto
- */
-template isUniformRNG(Rng)
-{
-    enum bool isUniformRNG = isInputRange!Rng &&
-        is(typeof(
-        {
-            static assert(Rng.isUniformRandom); //tag
-        }));
-}
-
-/**
- * Test if Rng is seedable. The overload
- * taking a SeedType also makes sure that the Rng can be seeded with SeedType.
- *
- * A seedable random-number generator has the following additional features:
- * $(UL
- *   $(LI it has a 'seed(ElementType)' function)
- * )
- */
-template isSeedable(Rng, SeedType)
-{
-    enum bool isSeedable = isUniformRNG!(Rng) &&
-        is(typeof(
-        {
-            Rng r = void;              // can define a Rng object
-            r.seed(SeedType.init);     // can seed a Rng
-        }));
-}
-
-///ditto
-template isSeedable(Rng)
-{
-    enum bool isSeedable = isUniformRNG!Rng &&
-        is(typeof(
-        {
-            Rng r = void;                     // can define a Rng object
-            r.seed(typeof(r.front).init);     // can seed a Rng
-        }));
-}
-
-@safe pure nothrow unittest
-{
-    struct NoRng
-    {
-        @property uint front() {return 0;}
-        @property bool empty() {return false;}
-        void popFront() {}
-    }
-    static assert(!isUniformRNG!(NoRng, uint));
-    static assert(!isUniformRNG!(NoRng));
-    static assert(!isSeedable!(NoRng, uint));
-    static assert(!isSeedable!(NoRng));
-
-    struct NoRng2
-    {
-        @property uint front() {return 0;}
-        @property bool empty() {return false;}
-        void popFront() {}
-
-        enum isUniformRandom = false;
-    }
-    static assert(!isUniformRNG!(NoRng2, uint));
-    static assert(!isUniformRNG!(NoRng2));
-    static assert(!isSeedable!(NoRng2, uint));
-    static assert(!isSeedable!(NoRng2));
-
-    struct NoRng3
-    {
-        @property bool empty() {return false;}
-        void popFront() {}
-
-        enum isUniformRandom = true;
-    }
-    static assert(!isUniformRNG!(NoRng3, uint));
-    static assert(!isUniformRNG!(NoRng3));
-    static assert(!isSeedable!(NoRng3, uint));
-    static assert(!isSeedable!(NoRng3));
-
-    struct validRng
-    {
-        @property uint front() {return 0;}
-        @property bool empty() {return false;}
-        void popFront() {}
-
-        enum isUniformRandom = true;
-    }
-    static assert(isUniformRNG!(validRng, uint));
-    static assert(isUniformRNG!(validRng));
-    static assert(!isSeedable!(validRng, uint));
-    static assert(!isSeedable!(validRng));
-
-    struct seedRng
-    {
-        @property uint front() {return 0;}
-        @property bool empty() {return false;}
-        void popFront() {}
-        void seed(uint val){}
-        enum isUniformRandom = true;
-    }
-    static assert(isUniformRNG!(seedRng, uint));
-    static assert(isUniformRNG!(seedRng));
-    static assert(isSeedable!(seedRng, uint));
-    static assert(isSeedable!(seedRng));
-}
 
 /**
 Linear Congruential generator.
