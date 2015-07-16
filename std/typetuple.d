134e78f783281e5c380505a464ceefa438ec33e4
Mike <slavo5150@yahoo.com>
2015-07-16 15:50:03 +0900

    Rename AliasTuple to Pack

--- std/typetuple.d
+++ std/typetuple.d
@@ -11,9 +11,9 @@ module std.typetuple;
 public import std.meta;
 
 /**
- * Alternate name for $(LREF AliasTuple) for legacy compatibility.
+ * Alternate name for $(LREF Pack) for legacy compatibility.
  */
-alias TypeTuple = AliasTuple;
+alias TypeTuple = Pack;
 
 ///
 unittest
