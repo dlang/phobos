35f1f6226aa523305e41f3dee68fbb0ce8fafdf4
k-hara <k.hara.pg@gmail.com>
2015-08-02 19:01:27 +0900

    Supplemental fix for issue 14860
    `task[] = RTask.init;` has two bugs:
    
    1. `RTask` is a nested struct, so `RTask.init` contains null context pointer.
    2. That is a block assignment, so there is a possibility to call
       `RTask.~this()` on garbage objects (stack allocated `buf` is initialized by `void`).
    
    Fixed to use `emplaceRef` on each elements.

--- std/parallelism.d
+++ std/parallelism.d
@@ -2624,7 +2624,8 @@ public:
                 }
             }
 
-            tasks[] = RTask.init;
+            foreach (ref t; tasks[])
+                emplaceRef(t, RTask());
 
             // Hack to take the address of a nested function w/o
             // making a closure.
