9aefe362964bd96516555e98bb4d5f9a5a663751
vladdeSV <v@vladde.net>
2015-08-15 12:33:06 +0200

    Change S to Char[]
    Replace std.conv.convFormat() with alias to std.format.format()

--- std/conv.d
+++ std/conv.d
@@ -27,10 +27,13 @@ import std.range.primitives;
 import std.traits;
 import std.typetuple;
 
-private S convFormat(S, Args...)(in S fmt, Args args) if (isSomeString!S)
+// Same as std.string.format, but "self-importing".
+// Helps reduce code and imports, particularly in static asserts.
+// Also helps with missing imports errors.
+package template convFormat()
 {
     import std.format : format;
-    return std.format.format(fmt, args);
+    alias convFormat = format;
 }
 
 /* ************* Exceptions *************** */
