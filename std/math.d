a09f116d6481b5ba8cf587ec95ab31292c648010
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2015-08-16 18:35:59 +0300

    Optimisation for LDC.
    Many functions from `std.math` and `std.mathspecial` use `poly`, so
    this PR affects significant part of math.
    
    remove pragmas

--- std/math.d
+++ std/math.d
@@ -6339,15 +6339,31 @@ unittest {
     static assert(poly(3.0, [1.0, 2.0, 3.0]) == 34);
 }
 
-private Unqual!(CommonType!(T1, T2)) polyImplBase(T1, T2)(T1 x, in T2[] A) @trusted pure nothrow @nogc
+private Unqual!(CommonType!(T1, T2)) polyImplBase(T1, T2)(in T1 x, in T2[] A) @trusted pure nothrow @nogc
     if (isFloatingPoint!T1 && isFloatingPoint!T2)
 {
     ptrdiff_t i = A.length - 1;
     typeof(return) r = A[i];
     while (--i >= 0)
     {
-        r *= x;
-        r += A[i];
+        version(LDC)
+        {
+            alias F = floatTraits!(typeof(return));
+            static if(F.realFormat == RealFormat.ieeeSingle || RealFormat.ieeeDouble)
+            {
+                r = llvm_fmuladd(r, x, A[i]);
+            }
+            else
+            {
+                r *= x;
+                r += A[i];
+            }
+        }
+        else
+        {
+            r *= x;
+            r += A[i];
+        }
     }
     return r;
 }
