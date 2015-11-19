74fbb79087c46ebde38ac3648b6a46b76f2a0a9e
Blumerline <yvpdgjyf@vomoto.com>
2015-11-19 14:14:06 +0100

    fix issue 15364 - BitArray.len should be private

--- std/bitmanip.d
+++ std/bitmanip.d
@@ -702,6 +702,9 @@ unittest
 
 struct BitArray
 {
+
+private:
+
     import std.format : FormatSpec;
     import core.bitop: bts, btr, bsf, bt;
 
@@ -709,7 +712,6 @@ struct BitArray
     size_t* ptr;
     enum bitsPerSizeT = size_t.sizeof * 8;
 
-private:
     @property size_t fullWords() const @nogc pure nothrow
     {
         return len / bitsPerSizeT;
