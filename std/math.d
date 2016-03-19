fd10beb59c3259662359124bf14a53d16434cbdf
Dan Olson <danole@comcast.net>
2016-03-19 21:49:34 +0000

    Fix getNaNPayload for 64-bit reals
    Shift was off-by-one.

--- std/math.d
+++ std/math.d
@@ -5491,7 +5491,7 @@ ulong getNaNPayload(real x) @trusted pure nothrow @nogc
         // Make it look like an 80-bit significand.
         // Skip exponent, and quiet bit
         m &= 0x0007_FFFF_FFFF_FFFF;
-        m <<= 10;
+        m <<= 11;
     }
     else static if (F.realFormat == RealFormat.ieeeQuadruple)
     {
