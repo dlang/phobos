329f068d7aedd0371b324c2676a4ebf75f3a4a13
Basile Burg <basile.b@gmx.com>
2016-04-09 10:01:51 +0200

    fix issue 15884 - Assigning char[] to std.json.JSONValue creates array, not string

--- std/json.d
+++ std/json.d
@@ -344,6 +344,11 @@ struct JSONValue
             type_tag = JSON_TYPE.STRING;
             store.str = arg;
         }
+        else static if(is(T : char[]))
+        {
+            type_tag = JSON_TYPE.STRING;
+            store.str = arg.idup;
+        }
         else static if(is(T : bool))
         {
             type_tag = arg ? JSON_TYPE.TRUE : JSON_TYPE.FALSE;
@@ -1674,4 +1679,11 @@ pure nothrow @safe @nogc unittest
     testVal = JSONValue[].init;
     testVal = null;
     assert(testVal.isNull);
+}
+
+pure nothrow @safe unittest // issue 15884
+{
+    char[] str = "a".dup;
+    JSONValue testVal = str;
+    assert(testVal.type == JSON_TYPE.STRING);
 }
