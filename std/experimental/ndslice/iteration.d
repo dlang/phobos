1c47dc26ed69249eae6b148478d3d7240192782f
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-04-17 07:31:28 +0200

    [ndslice] add deprecated files

---
+++ std/experimental/ndslice/iteration.d
@@ -0,0 +1,5 @@
+module std.experimental.ndslice.iteration;
+
+// @@@DEPRECATED_2017-06@@@
+deprecated("Please use sci.ndslice instead.")
+public import sci.ndslice.iteration;
---
+++ std/experimental/ndslice/package.d
@@ -0,0 +1,5 @@
+module std.experimental.ndslice;
+
+// @@@DEPRECATED_2017-06@@@
+deprecated("Please use sci.ndslice instead.")
+public import sci.ndslice;
