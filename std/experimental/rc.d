4033dba6e20467b77d0122139a7a51829bc3a91c
Andrei Alexandrescu <andrei@erdani.com>
2016-05-27 10:42:58 -0400

    WIP

--- std/experimental/rc.d
+++ std/experimental/rc.d
@@ -1,55 +1,211 @@
 module std.experimental.rc;
-import std.traits : isSomeChar;
+import std.traits : isSomeChar, isSomeString;
 
+import std.experimental.allocator.gc_allocator,
+    std.experimental.allocator.building_blocks.affix_allocator;
+version(unittest)
+    import std.experimental.allocator.building_blocks.stats_collector;
+version(unittest)
+{
+    private alias Alloc = StatsCollector!(
+        AffixAllocator!(GCAllocator, uint),
+        Options.bytesUsed
+    );
+    Alloc alloc;
+}
+else
+{
+    private alias Alloc = AffixAllocator!(GCAllocator, uint);
+    alias alloc = Alloc.instance;
+}
+
+private struct InSituStringCore(C)
+{
+    enum size_t capacity = 64 / C.sizeof - 1;
+    // state {
+    C[capacity] _payload = void;
+    ubyte _length = 0;
+    // } state
+
+    bool valid() const { return _length <= capacity; }
+    void invalidate() { _length = _length.max; }
+    size_t length() const { return _length; }
+    auto payloadPtr() inout { return _payload.ptr; }
+
+    void forceLength(size_t len)
+    {
+        assert(len <= capacity);
+        _length = cast(ubyte) len;
+    }
+}
+
+private struct CowStringCore(C)
+{
+    // state {
+    void[] _support;
+    C[] _payload;
+    // } state
+
+    size_t length() const
+    {
+        return _payload.length;
+    }
+
+    size_t capacity() const
+    {
+        return cast(C*) (_support.ptr + _support.length) - _payload.ptr;
+    }
+
+    unittest
+    {
+        CowStringCore t;
+        assert(t.capacity == 0);
+    }
+
+    auto payloadPtr() inout { return _payload.ptr; }
+
+    void forceLength(size_t len)
+    {
+        assert(len <= capacity);
+        _payload = _payload.ptr[0 .. len];
+    }
+
+    void reserve(size_t cap)
+    {
+        auto oldCapacity = capacity;
+        assert(cap > oldCapacity);
+        if (_support &&
+                alloc.expand(_support, C.sizeof * (cap - oldCapacity)))
+            return;
+        // Need to create a whole new string
+        auto t = cast(C[]) alloc.allocate(C.sizeof * cap);
+        t || assert(0);
+        t[0 .. _payload.length] = _payload[];
+        _payload = t[0 .. _payload.length];
+        _support = t;
+    }
+}
+
+private auto payload(RCStr)(ref RCStr s)
+{
+    return s.payloadPtr()[0 .. s.length];
+}
+
+private auto slack(RCStr)(ref RCStr s)
+{
+    return s.payloadPtr[s.length .. s.capacity];
+}
+
+/**
+*/
 struct RCStr(C)
 if (isSomeChar!C)
 {
-    import std.experimental.allocator.gc_allocator,
-        std.experimental.allocator.building_blocks.affix_allocator,
-        std.range;
-    alias Alloc = AffixAllocator!(GCAllocator, uint);
-    enum uint maxSmall = 64 / C.sizeof - 1;
-    union
+    import std.range, std.traits;
+    alias K = Unqual!C;
+
+    // state {
+    private union
     {
-        struct
-        {
-            C[maxSmall] small = void;
-            ubyte smallLen = 0;
-        }
-        struct
-        {
-            C[] payload;
-            size_t offset;
-        }
+        static assert(InSituStringCore!K.sizeof > CowStringCore!K.sizeof);
+        InSituStringCore!K _small;
+        CowStringCore!K _large = void;
+    }
+    // }
+
+    private bool isSmall() const { return _small.valid; }
+
+    private auto payload() inout
+    {
+        return isSmall ? _small.payload : _large.payload;
     }
 
+    /**
+    Returns the number of code units (e.g. bytes for UTF8) in the string.
+    */
+    size_t codeUnits() const
+    {
+        return isSmall ? _small.length : _large.length;
+    }
+
+    /**
+    Returns the number of code units (e.g. bytes for UTF8) in the string.
+    */
+    size_t capacity() const
+    {
+        return isSmall ? _small.capacity : _large.capacity;
+    }
+
+    /**
+    */
+    int opCmp(in RCStr!C rhs) const
+    {
+        return opCmp(rhs.payload);
+    }
+
+    /**
+    */
+    bool opEquals(in RCStr!C rhs)
+    {
+        return payload == rhs.payload;
+    }
+
+    /**
+    */
+    int opCmp(in K[] rhs) const
+    {
+        auto lhs = payload;
+        import std.algorithm.comparison : mismatch;
+        import std.string : representation;
+        auto r = mismatch(lhs.representation, rhs.representation);
+        if (r[0].empty) return r[1].empty ? 0 : -1;
+        if (r[1].empty) return 1;
+        // Both are non-empty
+        auto c0 = r[0][0], c1 = r[1][0];
+        return c0 < c1 ? -1 : (c0 > c1);
+    }
+
+    /**
+    */
+    bool opEquals(in C[] rhs)
+    {
+        return payload == rhs;
+    }
+
+    // Reserves at least `s` code units for this string.
     private void reserve(size_t s)
     {
-        immutable u = units;
-        if (s <= u) return;
-        if (u <= maxSmall)
+        if (!isSmall)
         {
-            if (s > maxSmall)
-            {
-                RCStr!C t;
-            }
+            // large to possibly larger
+            _large.reserve(s);
+            return;
         }
-        else
-        {
-
-        }
+        if (s <= _small.capacity) return;
+        // small to large
+        typeof(_large) t;
+        t.reserve(s);
+        assert(t.capacity > _small.length);
+        t.payloadPtr[0 .. _small.length] = _small.payload;
+        t.forceLength(_small.length);
+        import core.stdc.string, std.conv;
+        memcpy(&_large, &t, t.sizeof);
+        emplace(&t);
+        _small.invalidate;
     }
 
-    size_t units() const
+    RCStr!C opCat(X)(X x)
+    if (is(typeof(this ~= x)))
     {
-        if (smallLen <= maxSmall) return smallLen;
-        return payload.length - offset;
+        // TODO: optimize
+        auto result = this;
+        result ~= x;
+        return result;
     }
 
-    RCStr!C opCat(C1)(C1 c)
-    if (isSomeChar!C1)
+    void opCatAssign(in RCStr!C rhs)
     {
-        assert(0);
+        this ~= rhs.payload;
     }
 
     void opCatAssign(C1)(C1 c)
@@ -57,43 +213,41 @@ if (isSomeChar!C)
     {
         static if (C1.sizeof > C.sizeof)
         {
+            assert(0);
             static C[4] buf;
             import std.utf : encode;
-            auto l = encode(buf, c);
-            return this ~= buf[0 .. l];
+            auto len = encode(buf, c);
+            return this ~= buf[0 .. len];
         }
         else
         {
-            auto u = units;
-            if (u < maxSmall)
+            if (isSmall)
             {
-                small[u] = c;
-                ++smallLen;
-                return;
-            }
-            // Result is a large string
-            if (u == maxSmall)
-            {
-                // Small to large conversion
-                typeof(this) t;
-                t.reserve(units + 1);
-                t ~= this;
-                t ~= c;
-                import std.algorithm : move;
-                move(t, this);
+                if (_small.length < _small.capacity)
+                {
+                    // small to small
+                    _small.payloadPtr[_small.length] = c;
+                    _small.forceLength(_small.length + 1);
+                    assert(isSmall);
+                }
+                else
+                {
+                    // small to large
+                    reserve(_small.capacity + 1);
+                    assert(!isSmall);
+                    _large.payloadPtr[_large.length] = c;
+                    _large.forceLength(_large.length + 1);
+                }
             }
             else
             {
-                // Large stays large
-                assert(0);
+                // large to larger
+                if (_large.capacity == _large.length)
+                    _large.reserve(_large.capacity * 3 / 2);
+                _large.payloadPtr[_large.length] = c;
+                _large.forceLength(_large.length + 1);
             }
         }
-    }
-
-    void opCatAssign(RCStr!C rhs)
-    {
-        reserve(units + rhs.units);
-        assert(0);
     }
 
     void opCatAssign(R)(R r)
@@ -115,14 +269,51 @@ if (isSomeChar!C)
     }
 }
 
+version(unittest) private void test(C)()
+{
+    RCStr!C s;
+    assert(s == s);
+    assert(s <= s);
+    assert(!(s < s));
+    assert(s.codeUnits == 0);
+    assert(s.capacity == InSituStringCore!C.capacity);
+    s ~= '1';
+    assert(s.codeUnits == 1);
+    assert(s < "2");
+    assert(s > "");
+    assert(s > "0");
+    string x = "23456789012345678901234567890123456789012345678901234567890123";
+    s ~= x;
+    //import std.stdio;
+    //writeln(s.codeUnits);
+    assert(s.codeUnits == 63);
+    assert(s ==
+        "123456789012345678901234567890123456789012345678901234567890123");
+    s ~= '4';
+    assert(s.codeUnits == 64);
+    s ~= '5';
+    assert(s.codeUnits == 65);
+    auto s1 = s;
+    s ~= s;
+    assert(s.codeUnits == 130);
+    assert(s ==
+        "12345678901234567890123456789012345678901234567890123456789012345"
+        "12345678901234567890123456789012345678901234567890123456789012345");
+    auto s2 = s1 ~ s1;
+    assert(s2 == s);
+}
+
 unittest
 {
-    RCStr!char s;
-    assert(s.units == 0);
-    s ~= '1';
-    assert(s.units == 1);
-    s ~= "23456789012345678901234567890123456789012345678901234567890123";
-    assert(s.units == 63);
-    s ~= '4';
-    assert(s.units == 64);
+    test!char;
+    test!(const char);
+    test!(immutable char);
+    test!wchar;
+    test!(const wchar);
+    test!(immutable wchar);
+    test!dchar;
+    test!(const dchar);
+    test!(immutable dchar);
+    import std.stdio;
+    writeln(alloc.bytesUsed);
 }
