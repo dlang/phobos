c9674aa9de36c47ea180cfdc1149b82279476b5d
Bastiaan Veelo <veelo@users.noreply.github.com>
2016-06-06 00:14:28 +0200

    Fix 404.

--- std/experimental/allocator/building_blocks/kernighan_ritchie.d
+++ std/experimental/allocator/building_blocks/kernighan_ritchie.d
@@ -8,7 +8,7 @@ version(unittest) import std.conv : text;
 // KRRegion
 /**
 $(D KRRegion) draws inspiration from the $(LINK2
-std_experimental_allocator_region.html, region allocation strategy) and also the
+std_experimental_allocator_building_blocks_region.html, region allocation strategy) and also the
 $(WEB stackoverflow.com/questions/13159564/explain-this-implementation-of-malloc-from-the-kr-book,
 famed allocator) described by Brian Kernighan and Dennis Ritchie in section 8.7
 of the book $(WEB amazon.com/exec/obidos/ASIN/0131103628/classicempire, "The C
