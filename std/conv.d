dd8204550e003f425ea6ba8f3f0ffb8ef2af0025
Walter Bright <walter@walterbright.com>
2016-06-25 15:07:23 -0700

    std.conv: need cast(void) to address warning

--- std/conv.d
+++ std/conv.d
@@ -573,7 +573,7 @@ private T toImpl(T, S)(S value)
             T opCast(U)() @safe pure { assert(false); }
         }
     }
-    to!(Test.T)(Test.S());
+    cast(void)to!(Test.T)(Test.S());
 
     // make sure std.conv.to is doing the same thing as initialization
     Test.S s;
