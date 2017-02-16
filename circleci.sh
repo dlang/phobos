0f61f2ef939ee07b36374d77039d75f98eb2f9b2
Sebastian Wilzbach <seb@wilzba.ch>
2017-02-16 15:25:37 +0100

    Add fallback for CodeCov

--- circleci.sh
+++ circleci.sh
@@ -7,29 +7,37 @@ CURL_USER_AGENT="CirleCI $(curl --version | head -n 1)"
 DUB=${DUB:-$HOME/dlang/dub/dub}
 N=2
 CIRCLE_NODE_INDEX=${CIRCLE_NODE_INDEX:-0}
+CIRCLE_PROJECT_REPONAME=${CIRCLE_PROJECT_REPONAME:-phobos}
 
 case $CIRCLE_NODE_INDEX in
     0) MODEL=64 ;;
     1) MODEL=32 ;;
 esac
 
-install_deps() {
-    if [ $MODEL -eq 32 ]; then
-        sudo apt-get update
-        sudo apt-get install g++-multilib
-    fi
-
+download() {
+    local url="$1"
+    local fallbackurl="$2"
+    local outputfile="$3"
     for i in {0..4}; do
-        if curl -fsS -A "$CURL_USER_AGENT" --max-time 5 https://dlang.org/install.sh -O ||
-           curl -fsS -A "$CURL_USER_AGENT" --max-time 5 https://nightlies.dlang.org/install.sh -O ; then
+        if curl -fsS -A "$CURL_USER_AGENT" --max-time 5 "$url" -O "$outputfile" ||
+           curl -fsS -A "$CURL_USER_AGENT" --max-time 5 "$fallbackurl" -o "$outputfile" ; then
             break
         elif [ $i -ge 4 ]; then
             sleep $((1 << $i))
         else
-            echo 'Failed to download install script' 1>&2
+            echo "Failed to download script ${outputfile}" 1>&2
             exit 1
         fi
     done
+}
+
+install_deps() {
+    if [ $MODEL -eq 32 ]; then
+        sudo apt-get update --quiet=2
+        sudo aptitude install g++-multilib --assume-yes --quiet=2
+    fi
+
+    download "https://dlang.org/install.sh" "https://nightlies.dlang.org/install.sh" "install.sh"
 
     source "$(CURL_USER_AGENT=\"$CURL_USER_AGENT\" bash install.sh dmd-$HOST_DMD_VER --activate)"
     $DC --version
@@ -66,13 +74,13 @@ setup_repos()
 
     # merge upstream branch with changes, s.t. we check with the latest changes
     if [ -n "${CIRCLE_PR_NUMBER:-}" ]; then
-        local current_branch=$(git rev-parse --abbrev-ref HEAD)
-        git config user.name dummyuser
-        git config user.email dummyuser@dummyserver.com
-        git remote add upstream https://github.com/dlang/phobos.git
-        git fetch upstream
-        git checkout -f upstream/$base_branch
-        git merge -m "Automatic merge" $current_branch
+        local head=$(git rev-parse HEAD)
+        git fetch https://github.com/dlang/$CIRCLE_PROJECT_REPONAME.git $base_branch
+        git checkout -f FETCH_HEAD
+        local base=$(git rev-parse HEAD)
+        git config user.name 'CI'
+        git config user.email '<>'
+        git merge -m "Merge $head into $base" $head
     fi
 
     for proj in dmd druntime ; do
@@ -126,10 +134,19 @@ publictests()
     make -f posix.mak publictests DUB=$DUB
 }
 
+codecov()
+{
+    # CodeCov gets confused by lst files which it can't matched
+    rm -rf test/runnable/extra-files
+    download "https://codecov.io/bash" "https://raw.githubusercontent.com/codecov/codecov-bash/master/codecov" "codecov.sh"
+    bash codecov.sh
+}
+
 case $1 in
     install-deps) install_deps ;;
     setup-repos) setup_repos ;;
     coverage) coverage ;;
     style) style ;;
     publictests) publictests ;;
+    codecov) codecov ;;
 esac
