dlang/phobos

commit c6fb78ed14449725d0f5d10f38852eefb1e5dbfe

Small clean-up in std.conv.parse

std/conv.d
@@ -2341,15 +2341,17 @@ in
 body
 {
     import core.checkedint : mulu, addu;
+    import std.exception : enforce;
+
     if (radix == 10)
         return parse!Target(s);
 
+    enforce!ConvException(!s.empty, "s must not be empty in integral parse");
+
     immutable uint beyond = (radix < 10 ? '0' : 'a'-10) + radix;
-
     Target v = 0;
-    bool atStart = true;
 
-    for (; !s.empty; s.popFront())
+    do
     {
         uint c = s.front;
         if (c < '0')
@@ -2372,20 +2374,12 @@ body
 
         bool overflow = false;
         auto nextv = v.mulu(radix, overflow).addu(c - '0', overflow);
-        if (overflow || nextv > Target.max)
-            goto Loverflow;
+        enforce!ConvOverflowException(!overflow && nextv < Target.max, "Overflow in integral conversion");
         v = cast(Target) nextv;
+        s.popFront();
+    } while (!s.empty);
 
-        atStart = false;
-    }
-    if (atStart)
-        goto Lerr;
     return v;
-
-Loverflow:
-    throw new ConvOverflowException("Overflow in integral conversion");
-Lerr:
-    throw convError!(Source, Target)(s, radix);
 }
 
 @safe pure unittest

commit 044ad84eca029973c3299cc0f5c92a665ade107a

std.conv: Fix off-by-one regression in integer radix parse
Introduced in c6fb78ed14449725d0f5d10f38852eefb1e5dbfe (#5067).

std/conv.d
@@ -2401,7 +2401,7 @@ body
 
         bool overflow = false;
         auto nextv = v.mulu(radix, overflow).addu(c - '0', overflow);
-        enforce!ConvOverflowException(!overflow && nextv < Target.max, "Overflow in integral conversion");
+        enforce!ConvOverflowException(!overflow && nextv <= Target.max, "Overflow in integral conversion");
         v = cast(Target) nextv;
         s.popFront();
     } while (!s.empty);
@@ -2428,6 +2428,8 @@ body
 
     // 6609
     assert(parse!int(s = "-42", 10) == -42);
+
+    assert(parse!ubyte(s = "ff", 16) == 0xFF);
 }
 
 @safe pure unittest // bugzilla 7302