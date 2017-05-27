c4454773357a32c74227a16f322b4dba4f37004f
Andrey Kabylin <andrey@kabylin.ru>
2017-05-27 14:22:42 +0700

    rm redundant code, avoid double negation in names

--- std/traits.d
+++ std/traits.d
@@ -7660,18 +7660,14 @@ template getSymbolsByUDA(alias symbol, alias attribute) {
     }
 
     // filtering inaccessible members
-    enum noInaccessibleMembers(string name) = (__traits(compiles, __traits(getMember, symbol, name)));
-    alias withoutInaccessibleMembers = Filter!(noInaccessibleMembers, __traits(allMembers, symbol));
-
-    // filtering out nested class context
-    enum noThisMember(string name) = (name != "this");
-    alias membersWithoutNestedCC = Filter!(noThisMember, withoutInaccessibleMembers);
+    enum isAccessibleMember(string name) = __traits(compiles, __traits(getMember, symbol, name));
+    alias withoutInaccessibleMembers = Filter!(isAccessibleMember, __traits(allMembers, symbol));
 
     // filtering not compiled members such as alias of basic types
     enum hasSpecificUDA(string name) = mixin("hasUDA!(symbol." ~ name ~ ", attribute)");
-    enum noIncorrectMembers(string name) = (__traits(compiles, hasSpecificUDA!(name)));
+    enum isCorrectMember(string name) = __traits(compiles, hasSpecificUDA!(name));
 
-    alias withoutIncorrectMembers = Filter!(noIncorrectMembers, membersWithoutNestedCC);
+    alias withoutIncorrectMembers = Filter!(isCorrectMember, withoutInaccessibleMembers);
     alias membersWithUDA = toSymbols!(Filter!(hasSpecificUDA, withoutIncorrectMembers));
 
     // if the symbol itself has the UDA, tack it on to the front of the list
