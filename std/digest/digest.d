a0a0842b901d8346cd6cc60f4deddf16126d361a
Steven Schveighoffer <schveiguy@yahoo.com>
2017-08-06 17:21:49 -0400

    Fix issue 17724 - to be deprecated module std.digest.digest conflicts with contained function digest for alias.

--- std/digest/digest.d
+++ std/digest/digest.d
@@ -1,21 +1,21 @@
 module std.digest.digest;
 
-static import std.digest;
+import _newDigest = std.digest;
 
 // scheduled for deprecation in 2.077
 // See also: https://github.com/dlang/phobos/pull/5013#issuecomment-313987845
-alias isDigest = std.digest.isDigest;
-alias DigestType = std.digest.DigestType;
-alias hasPeek = std.digest.hasPeek;
-alias hasBlockSize = std.digest.hasBlockSize;
-alias digest = std.digest.digest;
-alias hexDigest = std.digest.hexDigest;
-alias makeDigest = std.digest.makeDigest;
-alias Digest = std.digest.Digest;
-alias Order = std.digest.Order;
-alias toHexString = std.digest.toHexString;
-alias asArray = std.digest.asArray;
-alias digestLength = std.digest.digestLength;
-alias WrapperDigest = std.digest.WrapperDigest;
-alias secureEqual = std.digest.secureEqual;
-alias LetterCase = std.digest.LetterCase;
+alias isDigest = _newDigest.isDigest;
+alias DigestType = _newDigest.DigestType;
+alias hasPeek = _newDigest.hasPeek;
+alias hasBlockSize = _newDigest.hasBlockSize;
+alias digest = _newDigest.digest;
+alias hexDigest = _newDigest.hexDigest;
+alias makeDigest = _newDigest.makeDigest;
+alias Digest = _newDigest.Digest;
+alias Order = _newDigest.Order;
+alias toHexString = _newDigest.toHexString;
+alias asArray = _newDigest.asArray;
+alias digestLength = _newDigest.digestLength;
+alias WrapperDigest = _newDigest.WrapperDigest;
+alias secureEqual = _newDigest.secureEqual;
+alias LetterCase = _newDigest.LetterCase;
