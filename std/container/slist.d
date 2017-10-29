fcc94e57f9733b1924e95d93b7a3e53bc1cbacbe
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2017-10-29 14:58:33 +0800

    Fix parens

--- std/container/slist.d
+++ std/container/slist.d
@@ -734,12 +734,12 @@ Complexity: $(BIGOH n)
     import std.algorithm.comparison : equal;
     
     auto a = SList!int(-1, 1, -2, 1, -3, 4);
-    assert(equal(a.consume!(e=>e<0),[-1,-2,-3]);
+    assert(equal(a.consume!(e=>e<0),[-1,-2,-3]));
     assert(equal(a[],[1,1,4]));
     
     auto b = SList!int(-1, 1, -2, 1, -3, 4);
     auto c = b[];
-    assert(equal(c.consume!(e=>e<0),[-1,-2,-3]);
+    assert(equal(c.consume!(e=>e<0),[-1,-2,-3]));
     assert(equal(b[],[1,1,4]));
     assert(equal(c[],[1,1,4]));
 }
