1d02ff487762510bbcfb22ef098a8fb460ad46e9
Sebastian Wilzbach <seb@wilzba.ch>
2017-12-24 11:25:15 +0100

    Fix typo

--- std/algorithm/searching.d
+++ std/algorithm/searching.d
@@ -1562,7 +1562,7 @@ if (isInputRange!InputRange &&
             }
         }
 
-        //Previous conditonal optimizations did not succeed. Fallback to
+        //Previous conditional optimizations did not succeed. Fallback to
         //unconditional implementations
         static if (isDefaultPred)
         {
