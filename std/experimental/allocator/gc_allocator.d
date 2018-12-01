1556befad7f0b562008cddbfdf695042515dab07
Rainer  Schuetze <r.sagitario@gmx.de>
2018-12-01 08:38:41 +0100

    gc_allocator.goodAllocSize: relax conditions for the actual GC to allow "good" sizes other than powers of 2

--- std/experimental/allocator/gc_allocator.d
+++ std/experimental/allocator/gc_allocator.d
@@ -170,10 +170,14 @@ pure @system unittest
 
         assert(GC.sizeOf(buffer.ptr) == s);
 
-        auto buffer2 = GCAllocator.instance.allocate(s - (s / 2) + 1);
-        scope(exit) () nothrow @nogc { GCAllocator.instance.deallocate(buffer2); }();
+        // the GC should provide power of 2 as "good" sizes, but other sizes are allowed, too
+        version (none)
+        {
+            auto buffer2 = GCAllocator.instance.allocate(s - (s / 2) + 1);
+            scope(exit) () nothrow @nogc { GCAllocator.instance.deallocate(buffer2); }();
 
-        assert(GC.sizeOf(buffer2.ptr) == s);
+            assert(GC.sizeOf(buffer2.ptr) == s);
+        }
     }
 
     // anything above a page is simply rounded up to next page
