0827012c3d08000bf75e3fa6d94d72f66d26f153
Lepadatu Tiberiu <tiberiulepadatu14@gmail.com>
2019-03-08 14:54:41 +0200

    Fix Issue 18728 - std.math.fdim does not handle nan correctly Add the real unittests. Changed the condition to match what thewilsonator said.

--- std/math.d
+++ std/math.d
@@ -7359,16 +7359,20 @@ T nextafter(T)(const T x, const T y) @safe pure nothrow @nogc
  *      $(TR $(TD x $(LT)= y) $(TD +0.0))
  *      )
  */
-real fdim(real x, real y) @safe pure nothrow @nogc { return (x > y) ? x - y : +0.0; }
+real fdim(real x, real y) @safe pure nothrow @nogc
+{
+    return (isNaN(x) || isNaN(y)) ? real.nan : ((x > y) ? x - y : +0.0);
+}
 
 ///
 @safe pure nothrow @nogc unittest
 {
     assert(fdim(2.0, 0.0) == 2.0);
     assert(fdim(-2.0, 0.0) == 0.0);
-    assert(fdim(real.infinity, 2.0) == real.infinity);
-    assert(fdim(real.nan, 2.0) == 0.0);
-    assert(fdim(2.0, real.nan) == 0.0);
+    assert(isInfinity(fdim(real.infinity, 2.0)));
+    assert(isNaN(fdim(real.nan, 2.0)));
+    assert(isNaN(fdim(2.0, real.nan)));
+    assert(isNaN(fdim(real.nan, real.nan)));
 }
 
 /**
