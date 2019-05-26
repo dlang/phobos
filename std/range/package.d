55328d6ef505338449be01b264b3aaa2388fb7c7
سليمان السهمي  (Suleyman Sahmi) <sahmi.soulaimane@gmail.com>
2019-05-26 02:17:07 +0000

    Ternary operator doesn't promote characters to integers anymore

--- std/range/package.d
+++ std/range/package.d
@@ -1274,8 +1274,8 @@ pure @safe nothrow unittest
     auto s3 = "string three".byCodeUnit;
     static assert(is(typeof(s3.front) == immutable char));
     auto r2 = s1.chain(s3);
-    //// chaining ranges of mixed character types gives `dchar`
-    //static assert(is(typeof(r2.front) == dchar));
+    // chaining ranges of mixed character types gives `dchar`
+    static assert(is(typeof(r2.front) == dchar));
 
     // use byChar on character ranges to correctly convert them to UTF-8
     auto r3 = s1.byChar.chain(s3);
