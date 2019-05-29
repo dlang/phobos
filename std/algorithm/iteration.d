7e549752cde73fb74b717e3dcdea51c9839ee1e4
valmat <ufabiz@gmail.com>
2019-05-29 15:50:17 +0500

    Removed falling test. Because it is no longer needed.

--- std/algorithm/iteration.d
+++ std/algorithm/iteration.d
@@ -179,28 +179,15 @@ if (isBidirectionalRange!Range)
         // http://en.wikipedia.org/wiki/Quartic_function
         return ( (x + 4.0) * (x + 1.0) * (x - 1.0) * (x - 3.0) ) / 14.0 + 0.5;
     }
-    // Without cache, with array (greedy)
-    auto result1 = iota(-4, 5).map!(a =>tuple(a, fun(a)))()
-                             .filter!(a => a[1] < 0)()
-                             .map!(a => a[0])()
-                             .array();
 
-    // the values of x that have a negative y are:
-    assert(equal(result1, [-3, -2, 2]));
-
-    // Check how many times fun was evaluated.
-    // As many times as the number of items in both source and result.
-    assert(counter == iota(-4, 5).length + result1.length);
-
-    counter = 0;
     // Without array, with cache (lazy)
-    auto result2 = iota(-4, 5).map!(a =>tuple(a, fun(a)))()
+    auto result = iota(-4, 5).map!(a =>tuple(a, fun(a)))()
                              .cache()
                              .filter!(a => a[1] < 0)()
                              .map!(a => a[0])();
 
     // the values of x that have a negative y are:
-    assert(equal(result2, [-3, -2, 2]));
+    assert(equal(result, [-3, -2, 2]));
 
     // Check how many times fun was evaluated.
     // Only as many times as the number of items in source.
