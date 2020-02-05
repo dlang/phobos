817602e479783632653cb364767744c4bb078276
Mark <mark@qtrac.eu>
2020-02-05 11:19:08 +0000

    Added a chmod example in the description...
    I added this because if you search the D docs for `chmod` no results appear. This should help with the search and also provide a useful example.

--- std/file.d
+++ std/file.d
@@ -2204,6 +2204,9 @@ if (isConvertibleToString!R)
 
 /++
     Set the _attributes of the given file.
+    
+    For example, a programmatic equivalent of Unix's `chmod +x name` to make a file executable is
+    `name.setAttributes(name.getAttributes | octal!700)`.
 
     Params:
         name = the file _name
