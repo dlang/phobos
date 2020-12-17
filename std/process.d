de8207f62d23bcb03c2c6ed72395b1b65d081f64
Vladimir Panteleev <git@thecybershadow.net>
2020-12-17 15:34:54 +0000

    std.process: Remove unfixable TODO
    Environment.remove cannot be @safe due to its use of the C functions
    SetEnvironmentVariableW and unsetenv, and doesn't have sufficient
    functionality to encapsulate the non-@safe fragments.

--- std/process.d
+++ std/process.d
@@ -322,7 +322,7 @@ static:
     multi-threaded programs. See e.g.
     $(LINK2 https://www.gnu.org/software/libc/manual/html_node/Environment-Access.html#Environment-Access, glibc).
     */
-    void remove(scope const(char)[] name) @trusted nothrow @nogc // TODO: @safe
+    void remove(scope const(char)[] name) @trusted nothrow @nogc
     {
         version (Windows)    SetEnvironmentVariableW(name.tempCStringW(), null);
         else version (Posix) core.sys.posix.stdlib.unsetenv(name.tempCString());
