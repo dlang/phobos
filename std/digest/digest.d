2f7155042afe99141fb3127f21752902c21ee590
Iain Buclaw <ibuclaw@gdcproject.org>
2021-02-01 18:16:39 +0100

    std.digest.digest: Update deprecation comment

--- std/digest/digest.d
+++ std/digest/digest.d
@@ -1,3 +1,3 @@
-// @@@DEPRECATED_2.091@@@
+// @@@DEPRECATED_2.101@@@
 deprecated("import std.digest instead of std.digest.digest. std.digest.digest will be removed in 2.101")
 module std.digest.digest;
