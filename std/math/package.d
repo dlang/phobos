94d951c093b32fbbfaff99bef4aba003125ea67b
berni44 <github@bernis-buecher.de>
2021-04-04 12:23:13 +0200

    std.math: Move constants into submodule.

--- std/math/package.d
+++ std/math/package.d
@@ -10,11 +10,22 @@ $(SCRIPT inhibitQuickIndex = 1;)
 $(DIVC quickindex,
 $(BOOKTABLE ,
 $(TR $(TH Category) $(TH Members) )
-$(TR $(TDNW Constants) $(TD
-    $(MYREF E) $(MYREF PI) $(MYREF PI_2) $(MYREF PI_4) $(MYREF M_1_PI)
-    $(MYREF M_2_PI) $(MYREF M_2_SQRTPI) $(MYREF LN10) $(MYREF LN2)
-    $(MYREF LOG2) $(MYREF LOG2E) $(MYREF LOG2T) $(MYREF LOG10E)
-    $(MYREF SQRT2) $(MYREF SQRT1_2)
+$(TR $(TDNW $(SUBMODULE Constants, constants)) $(TD
+    $(SUBREF constants, E)
+    $(SUBREF constants, PI)
+    $(SUBREF constants, PI_2)
+    $(SUBREF constants, PI_4)
+    $(SUBREF constants, M_1_PI)
+    $(SUBREF constants, M_2_PI)
+    $(SUBREF constants, M_2_SQRTPI)
+    $(SUBREF constants, LN10)
+    $(SUBREF constants, LN2)
+    $(SUBREF constants, LOG2)
+    $(SUBREF constants, LOG2E)
+    $(SUBREF constants, LOG2T)
+    $(SUBREF constants, LOG10E)
+    $(SUBREF constants, SQRT2)
+    $(SUBREF constants, SQRT1_2)
 ))
 $(TR $(TDNW Classics) $(TD
     $(MYREF abs) $(MYREF fabs) $(MYREF sqrt) $(MYREF cbrt) $(MYREF hypot)
@@ -105,6 +116,9 @@ $(TR $(TDNW Hardware Control) $(TD
  *      GT = &gt;
  *      SQRT = &radic;
  *      HALF = &frac12;
+ *
+ *      SUBMODULE = $(MREF_ALTTEXT $1, std, math, $2)
+ *      SUBREF = $(REF_ALTTEXT $(TT $2), $2, std, math, $1)$(NBSP)
  *
  * Copyright: Copyright The D Language Foundation 2000 - 2011.
  *            D implementations of tan, atan, atan2, exp, expm1, exp2, log, log10, log1p,
@@ -260,25 +274,6 @@ version (StdUnittest) private
     }
 }
 
-
-// Values obtained from Wolfram Alpha. 116 bits ought to be enough for anybody.
-// Wolfram Alpha LLC. 2011. Wolfram|Alpha. http://www.wolframalpha.com/input/?i=e+in+base+16 (access July 6, 2011).
-enum real E =          0x1.5bf0a8b1457695355fb8ac404e7a8p+1L; /** e = 2.718281... */
-enum real LOG2T =      0x1.a934f0979a3715fc9257edfe9b5fbp+1L; /** $(SUB log, 2)10 = 3.321928... */
-enum real LOG2E =      0x1.71547652b82fe1777d0ffda0d23a8p+0L; /** $(SUB log, 2)e = 1.442695... */
-enum real LOG2 =       0x1.34413509f79fef311f12b35816f92p-2L; /** $(SUB log, 10)2 = 0.301029... */
-enum real LOG10E =     0x1.bcb7b1526e50e32a6ab7555f5a67cp-2L; /** $(SUB log, 10)e = 0.434294... */
-enum real LN2 =        0x1.62e42fefa39ef35793c7673007e5fp-1L; /** ln 2  = 0.693147... */
-enum real LN10 =       0x1.26bb1bbb5551582dd4adac5705a61p+1L; /** ln 10 = 2.302585... */
-enum real PI =         0x1.921fb54442d18469898cc51701b84p+1L; /** &pi; = 3.141592... */
-enum real PI_2 =       PI/2;                                  /** $(PI) / 2 = 1.570796... */
-enum real PI_4 =       PI/4;                                  /** $(PI) / 4 = 0.785398... */
-enum real M_1_PI =     0x1.45f306dc9c882a53f84eafa3ea69cp-2L; /** 1 / $(PI) = 0.318309... */
-enum real M_2_PI =     2*M_1_PI;                              /** 2 / $(PI) = 0.636619... */
-enum real M_2_SQRTPI = 0x1.20dd750429b6d11ae3a914fed7fd8p+0L; /** 2 / $(SQRT)$(PI) = 1.128379... */
-enum real SQRT2 =      0x1.6a09e667f3bcc908b2fb1366ea958p+0L; /** $(SQRT)2 = 1.414213... */
-enum real SQRT1_2 =    SQRT2/2;                               /** $(SQRT)$(HALF) = 0.707106... */
-// Note: Make sure the magic numbers in compiler backend for x87 match these.
 
 /***********************************
  * Calculates the absolute value of a number.
