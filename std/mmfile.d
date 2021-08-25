213660d9bdda9deee628911fc8802401e259cc8c
Iain Buclaw <ibuclaw@gdcproject.org>
2021-08-25 15:58:08 +0200

    std.mmfile: Add testmmfile from dmd testsuite as a unittest

--- std/mmfile.d
+++ std/mmfile.d
@@ -725,3 +725,95 @@ version (linux)
     MmFile shar = new MmFile(null, MmFile.Mode.readWrite, 10, null, 0);
     void[] output = shar[0 .. $];
 }
+
+@system unittest
+{
+    import std.file : deleteme;
+    auto name = std.file.deleteme ~ "-test.tmp";
+    scope(exit) std.file.remove(name);
+
+    std.file.write(name, "abcd");
+    {
+        scope MmFile mmf = new MmFile(name);
+        string p;
+
+        assert(mmf[0] == 'a');
+        p = cast(string) mmf[];
+        assert(p[1] == 'b');
+        p = cast(string) mmf[0 .. 4];
+        assert(p[2] == 'c');
+    }
+    {
+        scope MmFile mmf = new MmFile(name, MmFile.Mode.read, 0, null);
+        string p;
+
+        assert(mmf[0] == 'a');
+        p = cast(string) mmf[];
+        assert(mmf.length == 4);
+        assert(p[1] == 'b');
+        p = cast(string) mmf[0 .. 4];
+        assert(p[2] == 'c');
+    }
+    std.file.remove(name);
+    {
+        scope MmFile mmf = new MmFile(name, MmFile.Mode.readWriteNew, 4, null);
+        char[] p = cast(char[]) mmf[];
+        p[] = "1234";
+        mmf[3] = '5';
+        assert(mmf[2] == '3');
+        assert(mmf[3] == '5');
+    }
+    {
+        string p = cast(string) std.file.read(name);
+        assert(p[] == "1235");
+    }
+    {
+        scope MmFile mmf = new MmFile(name, MmFile.Mode.readWriteNew, 4, null);
+        char[] p = cast(char[]) mmf[];
+        p[] = "5678";
+        mmf[3] = '5';
+        assert(mmf[2] == '7');
+        assert(mmf[3] == '5');
+        assert(cast(string) mmf[] == "5675");
+    }
+    {
+        string p = cast(string) std.file.read(name);
+        assert(p[] == "5675");
+    }
+    {
+        scope MmFile mmf = new MmFile(name, MmFile.Mode.readWrite, 4, null);
+        char[] p = cast(char[]) mmf[];
+        assert(cast(char[]) mmf[] == "5675");
+        p[] = "9102";
+        mmf[2] = '5';
+        assert(cast(string) mmf[] == "9152");
+    }
+    {
+        string p = cast(string) std.file.read(name);
+        assert(p[] == "9152");
+    }
+    std.file.remove(name);
+    {
+        scope MmFile mmf = new MmFile(name, MmFile.Mode.readWrite, 4, null);
+        char[] p = cast(char[]) mmf[];
+        p[] = "abcd";
+        mmf[2] = '5';
+        assert(cast(string) mmf[] == "ab5d");
+    }
+    {
+        string p = cast(string) std.file.read(name);
+        assert(p[] == "ab5d");
+    }
+    {
+        scope MmFile mmf = new MmFile(name, MmFile.Mode.readCopyOnWrite, 4, null);
+        char[] p = cast(char[]) mmf[];
+        assert(cast(string) mmf[] == "ab5d");
+        p[] = "9102";
+        mmf[2] = '5';
+        assert(cast(string) mmf[] == "9152");
+    }
+    {
+        string p = cast(string) std.file.read(name);
+        assert(p[] == "ab5d");
+    }
+}
