14c3a6ed3c6623706ac26b28d93da45516fe8e6e
Per Nordlöw <per.nordlow@gmail.com>
2021-09-21 00:10:47 +0200

    Move TODO of formatValue T template parameter

--- std/format/write.d
+++ std/format/write.d
@@ -1228,8 +1228,9 @@ Note:
 See_Also:
     $(LREF formattedWrite) which formats several values at once.
  */
-void formatValue(Writer, T, Char)(auto ref Writer w, auto ref T val, scope const ref FormatSpec!Char f) // TODO: make const(T) work
-{
+void formatValue(Writer, T, Char)(auto ref Writer w, auto ref T val, scope const ref FormatSpec!Char f)
+{ // TODO: make const(T) work. this template is currently instantiated 10890 times when make -f posix.mak unittest
+    pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", T);
     import std.format : enforceFmt;
     import std.traits : isIntegral;
 
