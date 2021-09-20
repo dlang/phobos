91d9a1dbca8155d9ac815e3d46fa377279671361
Per Nordlöw <per.nordlow@gmail.com>
2021-09-21 01:04:51 +0200

    Add TODO comment

--- std/format/write.d
+++ std/format/write.d
@@ -1242,6 +1242,12 @@ void formatValue(Writer, T, Char)(auto ref Writer w, auto ref T val, scope const
         formatValueImpl(w, val, f);
     else static if (is(immutable X == immutable U, U) && is(U == char) || is(U == wchar) || is(U == dchar))
         formatValueImplChar(w, val, f);
+    // TODO:
+    // else static if (is(immutable T == immutable C[], C) && (is(C == char) || is(C == wchar) || is(C == dchar)))
+    // {
+    //     pragma(msg, __FILE__, "(", __LINE__, ",1): Debug: ", T);
+    //     formatRange(w, cast(const)val, f);
+    // }
     else static if (is(immutable X == immutable U, U) && is(U == bool))
         formatValueImplBool(w, val, f);
     else static if (isIntegral!T)
