574e001d3564d2dfb5f4f76e7b44bf914a547f75
Carsten Schlote <schlote@vahanus.net>
2022-04-29 17:26:56 +0200

    Fixed linter issue on style

--- std/outbuffer.d
+++ std/outbuffer.d
@@ -225,13 +225,13 @@ class OutBuffer
     @safe unittest
     {
         OutBuffer buf = new OutBuffer();
-        buf.write(cast(ubyte)1);
+        buf.write(cast(ubyte) 1);
         buf.align2();
         assert(buf.toBytes() == "\x01\x00");
-        buf.write(cast(ubyte)2);
+        buf.write(cast(ubyte) 2);
         buf.align4();
         assert(buf.toBytes() == "\x01\x00\x02\x00");
-        buf.write(cast(ubyte)3);
+        buf.write(cast(ubyte) 3);
         buf.alignSize(8);
         assert(buf.toBytes() == "\x01\x00\x02\x00\x03\x00\x00\x00");
     }
@@ -239,13 +239,13 @@ class OutBuffer
     @safe unittest
     {
         OutBuffer buf = new OutBuffer();
-        buf.write(cast(ubyte)1);
+        buf.write(cast(ubyte) 1);
         buf.align2(0x55);
         assert(buf.toBytes() == "\x01\x55");
-        buf.write(cast(ubyte)2);
+        buf.write(cast(ubyte) 2);
         buf.align4(0x55);
         assert(buf.toBytes() == "\x01\x55\x02\x55");
-        buf.write(cast(ubyte)3);
+        buf.write(cast(ubyte) 3);
         buf.alignSize(8, 0x55);
         assert(buf.toBytes() == "\x01\x55\x02\x55\x03\x55\x55\x55");
     }
