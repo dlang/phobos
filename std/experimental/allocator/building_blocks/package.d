14eb9f8432a9cd785b55f1a66694e00fc4ce6209
Nick Treleaven <ntrel002@gmail.com>
2022-10-16 16:59:06 +0100

    Replace broken BitmappedBlock example with Region

--- std/experimental/allocator/building_blocks/package.d
+++ std/experimental/allocator/building_blocks/package.d
@@ -149,6 +149,9 @@ block size of 4 KB. Sizes above that are passed direct to the `GCAllocator`.
 
 $(RUNNABLE_EXAMPLE
     ----
+    import std.experimental.allocator;
+    import std.algorithm.comparison : max;
+
     alias FList = FreeList!(GCAllocator, 0, unbounded);
     alias A = Segregator!(
         8, FreeList!(GCAllocator, 0, 8),
@@ -158,8 +161,7 @@ $(RUNNABLE_EXAMPLE
         1024, Bucketizer!(FList, 513, 1024, 128),
         2048, Bucketizer!(FList, 1025, 2048, 256),
         3584, Bucketizer!(FList, 2049, 3584, 512),
-        4072 * 1024, AllocatorList!(
-            () => BitmappedBlock!(GCAllocator, 4096)(4072 * 1024)),
+        4072 * 1024, AllocatorList!(n => Region!GCAllocator(max(n, 1024 * 4096))),
         GCAllocator
     );
     A tuMalloc;
