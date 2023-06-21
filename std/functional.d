6e2d38fce9d079cb00ed5b7d5c96dd995f888c74
Quirin F. Schroll <Bolpat@users.noreply.github.com>
2023-06-21 12:16:21 +0200

    Respect linkage

--- std/functional.d
+++ std/functional.d
@@ -1718,8 +1718,16 @@ if (isCallable!(F))
     }
     else static if (is(F Func == Func*) && is(Func == function) && is(Func Params == __parameters))
     {
-        alias dg = delegate(Params params) const => F.init(params);
-        typeof(dg) result; // inlining `dg` infers attributes incorrectly
+        // https://issues.dlang.org/show_bug.cgi?id=24007 - cannot specify linkage on function literal:
+        //alias dg = delegate(Params params) const => F.init(params);
+        //typeof(dg) result;
+		// Workaround:
+        struct S
+        {
+            mixin("extern(", __traits(getLinkage, fp), ") auto ref dg(Params params) const => F.init(params);");
+        }
+        typeof(&S().dg) result; // inlining `dg` infers attributes incorrectly
+
         () @trusted
         {
             // assigning funcptr is @system, but it’s safe here because `fp` needs no context
@@ -1734,7 +1742,7 @@ if (isCallable!(F))
     }
     else
     {
-        alias DelType = typeof(&(new DelegateFaker!(F)).doIt);
+        alias DelType = typeof(&(new DelegateFaker!F).doIt);
 
         static struct DelegateFields {
             union {
@@ -1755,7 +1763,7 @@ if (isCallable!(F))
 
         df.contextPtr = cast(void*) fp;
 
-        DelegateFaker!(F) dummy;
+        DelegateFaker!F dummy;
         auto dummyDel = &dummy.doIt;
         df.funcPtr = dummyDel.funcptr;
 
