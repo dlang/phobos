38ba482e7b450e20536a4a62de370b3339f37482
Vladiwostok <rickvsprot@protonmail.com>
2024-10-26 16:17:39 +0300

    Fix old (libdparse) D-Scanner linting warn

--- std/path.d
+++ std/path.d
@@ -1446,8 +1446,7 @@ private auto _withDefaultExtension(R, C)(R path, C[] ext)
         of segments to assemble the path from.
     Returns: The assembled path.
 */
-immutable(ElementEncodingType!(ElementType!Range))[]
-    buildPath(Range)(scope Range segments)
+immutable(ElementEncodingType!(ElementType!Range))[] buildPath(Range)(scope Range segments)
 if (isInputRange!Range && !isInfinite!Range && isSomeString!(ElementType!Range))
 {
     if (segments.empty) return null;
