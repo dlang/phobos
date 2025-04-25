f0e8109f722a528bc15aa87b1087b95318aa90a7
Akshat Sharma <aksharma.nsut@gmail.com>
2025-04-25 09:13:19 +0530

    Update typecons.d
    Format as per D style

--- std/typecons.d
+++ std/typecons.d
@@ -3728,7 +3728,7 @@ struct Nullable(T)
     @safe unittest
     {
         Nullable!int a = 5;
-        Nullable!int b = Nullable!int.init; // null
+        Nullable!int b = Nullable!int.init;
 
         assert(a > b);
         assert(b < a);
