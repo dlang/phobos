86a7c59c5047616a88f588fbfa6ea93a0af130d2
Georgy Markov <ogion.art@gmail.com>
2025-06-07 23:50:16 +0300

    Works on my machine but CI reports missing import

--- std/numeric.d
+++ std/numeric.d
@@ -3489,6 +3489,7 @@ public:
     ///
     unittest
     {
+        import std.complex : Complex;
         enum size = 8;
         float[size] arr = [1,2,3,4,5,6,7,8];
         Complex!float[size] fft1;
