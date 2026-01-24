440db1ea5b164cd47c22a5a2a24c39296f8df304
Richard (Rikki) Andrew Cattermole <richard@cattermole.co.nz>
2026-01-25 12:22:37 +1300

    Fix potential null check error in Socket.select (#10939)

--- std/socket.d
+++ std/socket.d
@@ -3510,7 +3510,7 @@ public:
             if (checkError) checkError.setMinCapacity(n);
         }
 
-        int result = .select(n, fr, fw, fe, &timeout.ctimeval);
+        int result = .select(n, fr, fw, fe, timeout !is null ? &timeout.ctimeval : null);
 
         version (Windows)
         {
