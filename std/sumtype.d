ee5eb80a36c3d5c50c2232b2bde5a301a6be94c1
Niyati Jain <niyati.jainn15@gmail.com>
2026-07-19 18:51:17 +0530

    Restore arity-based string handlers and document 3+ limit
    Use unaryFun/binaryFun from handlerAlias based on SumType argument count
    (dispatch arity), with a clear error for string handlers on 3+ arguments.
    Document that string lambdas follow std.functional and are not supported
    beyond two SumTypes.

--- std/sumtype.d
+++ std/sumtype.d
@@ -1698,7 +1698,11 @@ enum bool isSumType(T) = is(T : SumType!Args, Args...);
  * overloads are considered as potential matches.
  *
  * String handlers such as `"a.member"` are accepted via
- * $(REF unaryFun, std,functional), consistent with the rest of Phobos.
+ * $(REF unaryFun, std,functional) when matching one [SumType], or
+ * $(REF binaryFun, std,functional) when matching two, consistent with the rest
+ * of Phobos. String handlers are not supported when matching three or more
+ * [SumType]s; use a function literal in that case. (This mirrors
+ * `std.functional`, which provides only unary and binary string adapters.)
  *
  * Templated handlers are also accepted, and will match any type for which they
  * can be [implicitly instantiated](https://dlang.org/glossary.html#ifti).
@@ -1742,6 +1746,13 @@ version (D_BetterC) {} else
     alias Sum = SumType!(Foo, Bar);
 
     assert(Sum(Bar(3)).match!"a.value" == 3);
+
+    // Two SumType arguments use binaryFun ("a", "b")
+    assert(match!"a.value + b.value"(Sum(Foo(2)), Sum(Bar(3))) == 5);
+
+    // String handlers are not supported for 3+ SumType arguments
+    alias S = SumType!int;
+    static assert(!__traits(compiles, match!"a + b + c"(S(1), S(2), S(3))));
 }
 
 /** $(DIVID avoiding-unintentional-matches, $(H3 Avoiding unintentional matches))
@@ -1971,13 +1982,39 @@ private template Iota(size_t n)
     assert(Iota!3 == AliasSeq!(0, 1, 2));
 }
 
+/* Adapt handlers with unaryFun/binaryFun when matching 1/2 SumTypes.
+ *
+ * `nArgs` is the number of SumType *arguments* passed to match (dispatch
+ * arity), not the number of mutually exclusive member types in a SumType.
+ * std.functional has no n-ary string adapter, so string handlers are rejected
+ * for 3+ arguments.
+ */
+private template handlerAlias(alias handler, size_t nArgs)
+{
+    import std.functional : binaryFun, unaryFun;
+
+    static if (nArgs == 1)
+        alias handlerAlias = unaryFun!handler;
+    else static if (nArgs == 2)
+        alias handlerAlias = binaryFun!handler;
+    else
+    {
+        static if (is(typeof(handler) : string))
+        {
+            static assert(0,
+                "String handlers are only supported when matching 1 or 2 " ~
+                "SumTypes; use a function literal for 3 or more.");
+        }
+        else
+            alias handlerAlias = handler;
+    }
+}
+
 private template matchImpl(Flag!"exhaustive" exhaustive, handlers...)
 {
     auto ref matchImpl(SumTypes...)(auto ref SumTypes args)
     if (allSatisfy!(isSumType, SumTypes) && args.length > 0)
     {
-        import std.functional : unaryFun;
-
         // Single dispatch (fast path)
         static if (args.length == 1)
         {
@@ -2050,7 +2087,8 @@ private template matchImpl(Flag!"exhaustive" exhaustive, handlers...)
             {
                 static foreach (hid, handler; handlers)
                 {
-                    static if (canMatch!(unaryFun!handler, valueTypes!caseId))
+                    static if (canMatch!(handlerAlias!(handler, SumTypes.length),
+                            valueTypes!caseId))
                     {
                         if (result[caseId] == noMatch)
                         {
@@ -2083,7 +2121,8 @@ private template matchImpl(Flag!"exhaustive" exhaustive, handlers...)
 
         static foreach (size_t hid, handler; handlers)
         {
-            mixin("alias ", handlerName!hid, " = unaryFun!handler;");
+            mixin("alias ", handlerName!hid, " = handlerAlias!(handler, ",
+                toCtString!(SumTypes.length), ");");
         }
 
         // Single dispatch (fast path)
